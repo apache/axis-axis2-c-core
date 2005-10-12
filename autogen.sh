5c7ac403176fc9e32ef0fcb87d3e18721acf6be7
Damitha N.M. Kumarage <damitha@apache.org>
2005-10-12 05:20:58 +0000

    Added gnu autobuild system.

---
+++ autogen.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "run libtoolize."          && 
+libtoolize --force              && 
+echo "run aclocal."             && 
+aclocal                         && 
+echo "run autoconf."            && 
+autoconf                        && 
+echo "run autoheader."          && 
+autoheader                      && 
+echo "run automake."            && 
+automake --add-missing --gnu
