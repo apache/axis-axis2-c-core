23be5e668b8541e093c44b633a2bf31eaa91967d
Damitha N.M. Kumarage <damitha@apache.org>
2005-10-13 03:34:51 +0000

    added main unit test suite

---
+++ modules/test/unit/main.c
@@ -0,0 +1,19 @@
+#include <CuTest.h>
+#include "../../common/test/common_test.h"
+
+void RunAllTests(void) {
+    CuString *output = CuStringNew();
+    CuSuite* suite = CuSuiteNew();
+
+    CuSuiteAddSuite(suite, axis2_commonGetSuite());
+
+    CuSuiteRun(suite);
+    CuSuiteSummary(suite, output);
+    CuSuiteDetails(suite, output);
+    printf("%s\n", output->buffer);
+}
+
+int main(void) {
+    RunAllTests();
+    return 0;
+}
