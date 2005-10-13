82d268e3cbcf3acca36cdc7c71482f0954d224a6
Damitha N.M. Kumarage <damitha@apache.org>
2005-10-13 04:20:20 +0000

    These files are best placed here

---
+++ modules/util/src/axis2_environment.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2004,2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <axis2_environment.h>
+
+axis2_environment_t *axis2_environment_create(axis2_allocator_t *allocator, axis2_error_t *error, axis2_stream_t *stream, axis2_log_t *log)
+{
+    if (!allocator)
+        return NULL;
+
+    axis2_environment_t *environment = (axis2_environment_t*)axis2_malloc(allocator, sizeof(axis2_environment_t));
+
+    if (!environment)
+        return NULL;
+
+    environment->allocator = allocator;
+
+    if (!error)
+        environment->error = axis2_error_create(allocator);
+
+    if (!stream)
+        environment->stream = axis2_stream_create(allocator, NULL);
+
+    if (!log)
+        environment->log = axis2_log_create(allocator, NULL);
+
+    return environment;
+}
+
