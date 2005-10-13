e45ec94932bbd1501834be41baeb188b3a6cca64
Damitha N.M. Kumarage <damitha@apache.org>
2005-10-13 12:04:35 +0000

    added missing file

---
+++ modules/common/test/main.c
@@ -0,0 +1,18 @@
+#include "common_test.h"
+
+void RunAllTests(void) {
+    CuString *output = CuStringNew();
+    CuSuite* suite = CuSuiteNew();
+
+    CuSuiteAddSuite(suite, axis2_commonGetSuite());
+
+    CuSuiteRun(suite);
+    CuSuiteSummary(suite, output);
+    CuSuiteDetails(suite, output);
+    printf("%s\n", output->buffer);
+}
+
+int main(void) {
+    RunAllTests();
+    return 0;
+}
