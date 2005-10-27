87399eb9044c029a1b5289ee90246523e543f185
Damitha N.M. Kumarage <damitha@apache.org>
2005-10-27 10:08:16 +0000

    Added files required to add unit tests for om

--- modules/xml/om/test/om_test.c
+++ modules/xml/om/test/om_test.c
@@ -1,47 +1,12 @@
-#include <CuTest.h>
+#include "om_test.h"
 #include <string.h>
 #include <stdio.h>
-#include <axis2_om_node.h>
+#include <axis2_allocator.h>
+#include <axis2_environment.h>
 
-void Testnode_detach(CuTest *tc) {
-	axis2_om_node_t* parent = axis2_om_node_create();
-    axis2_om_node_t* prev_sibling = axis2_om_node_create();
-    axis2_om_node_add_child(parent, prev_sibling);
-    axis2_om_node_t* test_node = axis2_om_node_create();
-    axis2_om_node_add_child(parent, test_node);
-    axis2_om_node_t* next_sibling = axis2_om_node_create();
-    axis2_om_node_add_child(parent, next_sibling);
-        
-    axis2_om_node_t* temp_parent = axis2_om_node_detach(test_node);
-    puts("came");
-    if(0 == temp_parent) puts("parent is null\n");
-    axis2_om_node_t* expected = temp_parent->first_child;
-    printf("came2");
-    if(0 == expected) puts("expected is null\n");
-        axis2_om_node_t* actual = next_sibling;
-
-    CuAssertPtrEquals(tc, expected, actual); 
-}
-
-CuSuite* node_detachGetSuite() {
+CuSuite* axis2_omGetSuite() {
     CuSuite* suite = CuSuiteNew();
-    SUITE_ADD_TEST(suite, Testnode_detach);
+    /*SUITE_ADD_TEST(suite, Testaxis2_stream_ops_read);*/
     return suite;
 }
 
- void RunAllTests(void) {
-    CuString *output = CuStringNew();
-    CuSuite* suite = CuSuiteNew();
-
-    CuSuiteAddSuite(suite, node_detachGetSuite());
-
-    CuSuiteRun(suite);
-    CuSuiteSummary(suite, output);
-    CuSuiteDetails(suite, output);
-    printf("%s\n", output->buffer);
-}
-
-int main(void) {
-    RunAllTests();
-    return 0;
-}
---
+++ modules/xml/om/test/om_test.h
@@ -0,0 +1,12 @@
+#ifndef OM_TEST_H
+#define OM_TEST_H
+
+#include <CuTest.h>
+#include "om_stream_test.h"
+#include "om_log_test.h"
+#include "om_hash_test.h"
+#include "om_error_test.h"
+
+CuSuite* axis2_omGetSuite();
+
+#endif /* OM_TEST_H */
