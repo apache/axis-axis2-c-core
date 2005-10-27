cb163a42076c73c2d7c95670f875675c57b7ac76
Damitha N.M. Kumarage <damitha@apache.org>
2005-10-27 11:00:09 +0000

    build files updated

--- modules/xml/om/test/main.c
+++ modules/xml/om/test/main.c
@@ -1,13 +1,9 @@
-#include <CuTest.h>
-#include "../../util/test/util_test.h"
-#include "../../common/test/common_test.h"
+#include "om_test.h"
 
 void RunAllTests(void) {
     CuString *output = CuStringNew();
     CuSuite* suite = CuSuiteNew();
 
-    CuSuiteAddSuite(suite, axis2_utilGetSuite());
-    CuSuiteAddSuite(suite, axis2_commonGetSuite());
     CuSuiteAddSuite(suite, axis2_omGetSuite());
 
     CuSuiteRun(suite);
