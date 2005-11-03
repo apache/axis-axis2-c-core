743594e3fab833f20bcdfdf6dd05876bd4c981dc
Damitha N.M. Kumarage <damitha@apache.org>
2005-11-03 05:59:35 +0000

    changed the env create function according to the new changes in env

--- modules/util/test/util_log_test.c
+++ modules/util/test/util_log_test.c
@@ -5,7 +5,7 @@ void Testaxis2_log_write(CuTest *tc)
 
     char actual[10];
     axis2_allocator_t *allocator = axis2_allocator_init(NULL);
-    axis2_env_t *env = axis2_environment_create(allocator, NULL, NULL, NULL);
+    axis2_env_t *env = axis2_environment_create(allocator);
     char *expected = strdup("aaaaaaaaa");
     AXIS2_STREAM_READ(env->stream, actual, 10);
     AXIS2_LOG_WRITE(env->log, actual, 10);
--- modules/util/test/util_stream_test.c
+++ modules/util/test/util_stream_test.c
@@ -4,7 +4,7 @@ void Testaxis2_stream_read(CuTest *tc) {
 
     char actual[10];
     axis2_allocator_t *allocator = axis2_allocator_init(NULL);
-    axis2_env_t *env = axis2_environment_create(allocator, NULL, NULL, NULL);
+    axis2_env_t *env = axis2_environment_create(allocator);
     char *expected = (char*) strdup("aaaaaaaaa");
     AXIS2_STREAM_READ(env->stream, actual, 10);
     CuAssertStrEquals(tc, expected, actual);
@@ -14,7 +14,7 @@ void Testaxis2_stream_write(CuTest *tc) {
 
     char actual[10];
     axis2_allocator_t *allocator = axis2_allocator_init(NULL);
-    axis2_env_t *env = axis2_environment_create(allocator, NULL, NULL, NULL);
+    axis2_env_t *env = axis2_environment_create(allocator);
     char *expected = (char*) strdup("aaaaaaaaa");
     AXIS2_STREAM_READ(env->stream, actual, 10);
     AXIS2_STREAM_WRITE(env->stream, actual, 10);
