fa83c85d74853025c084a7b62a71bc9302708b8f
Damitha N.M. Kumarage <damitha@apache.org>
2005-11-07 04:43:33 +0000

    Parameter order changed for macro AXIS2_ERROR_HANDLE AND AXIS2_PARAM_CHECK

--- include/axis2.h
+++ include/axis2.h
@@ -42,7 +42,7 @@
  * @return If function return a status code return AXIS2_SUCCESS. Else if
  *         function return a type pointer return NULL
  */
-#define AXIS2_PARAM_CHECK(env, object, error_return) \
+#define AXIS2_PARAM_CHECK(object, env, error_return) \
     if (!object) \
     { \
         AXIS2_ERROR_SET_ERROR_NUMBER((*env)->error, AXIS2_ERROR_INVALID_NULL_PARAM); \
@@ -62,7 +62,7 @@
  * @return If function return a status code return AXIS2_SUCCESS. Else if
  *         function return a type pointer return NULL
  */    
-#define AXIS2_ERROR_SET(env, error_number, error_return) \
+#define AXIS2_ERROR_SET(error_number, env, error_return) \
     { \
         AXIS2_ERROR_SET_ERROR_NUMBER((*env)->error, AXIS2_ERROR_INVALID_NULL_PARAM); \
         AXIS2_ERROR_SET_STATUS_CODE((*env)->error, AXIS2_FAILURE); \
