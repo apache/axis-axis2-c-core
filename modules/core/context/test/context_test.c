ce8821da739d6f9021afd3062af490d0d6d70d18
Damitha N.M. Kumarage <damitha@apache.org>
2005-11-16 09:08:38 +0000

    Added test case folders

--- modules/core/context/test/context_test.c
+++ modules/core/context/test/context_test.c
@@ -1,10 +1,10 @@
-#include "engine_test.h"
+#include "context_test.h"
 #include <string.h>
 #include <stdio.h>
 #include <axis2_allocator.h>
 #include <axis2_env.h>
 
-CuSuite* axis2_engineGetSuite() {
+CuSuite* axis2_contextGetSuite() {
     CuSuite* suite = CuSuiteNew();
     /*SUITE_ADD_TEST(suite, Testaxis2_stream_ops_read);*/
     return suite;
