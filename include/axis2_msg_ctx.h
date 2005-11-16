60b68354f85d93b59c4682edf6146e9513aaafbc
Don Samisa Abeysinghe <samisa@apache.org>
2005-11-16 04:02:14 +0000

    Added relates_to

--- include/axis2_msg_ctx.h
+++ include/axis2_msg_ctx.h
@@ -24,6 +24,7 @@
   */
 
 #include <axis2_core.h>
+#include <axis2_relates_to.h>
 
 #ifdef __cplusplus
 extern "C"
@@ -61,7 +62,7 @@ struct axis2_msg_ctx_ops
     axis2_bool_t (AXIS2_CALL *is_paused)(axis2_msg_ctx_t *msg_ctx,
                                          axis2_env_t **env); 
     /** TODO change void* to real return type */
-    void* (AXIS2_CALL *get_relates_to)(axis2_msg_ctx_t *msg_ctx,
+    axis2_relates_to_t* (AXIS2_CALL *get_relates_to)(axis2_msg_ctx_t *msg_ctx,
                                          axis2_env_t **env); 
 };
 
