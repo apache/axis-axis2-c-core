21d4cf1583700072a900f2656c062059e071ddc7
Damitha N.M. Kumarage <damitha@apache.org>
2005-12-29 06:12:16 +0000

    Removed common folder because it is not used in the project

--- modules/common/src/axis2.c
+++
@@ -1,1 +0,0 @@
-#include <axis2.h>
--- modules/common/test/common_test.c
+++
@@ -1,9 +0,0 @@
-#include "common_test.h"
-
-CuSuite* axis2_commonGetSuite() {
-    CuSuite* suite = CuSuiteNew();
-    /*SUITE_ADD_TEST(suite, Testaxis2_stream_ops_read);*/
-    /*SUITE_ADD_TEST(suite, Testaxis2_stream_ops_write);*/
-    return suite;
-}
-
