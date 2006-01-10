a44f29c8df941566ceadddda512ec5f5c30cac0e
Damitha N.M. Kumarage <damitha@apache.org>
2006-01-10 09:52:05 +0000

    make some changes

--- test/core/engine/test_engine.c
+++ test/core/engine/test_engine.c
@@ -2,6 +2,7 @@
 #include <axis2_env.h>
 #include <axis2_engine.h>
 #include <axis2_conf_ctx.h>
+#include <axis2_msg_ctx.h>
 
 int	axis2_test_engine_send()
 {
@@ -13,12 +14,33 @@ int	axis2_test_engine_send()
 
     struct axis2_conf_ctx *conf_ctx;
 	struct axis2_msg_ctx *msg_ctx;
+	struct axis2_op_ctx *op_ctx;
+	struct axis2_op *op;
+	struct axis2_svc *svc;
+	struct axis2_svc_ctx *svc_ctx;
+	struct axis2_svc_grp *svc_grp;
+	struct axis2_svc_grp_ctx *svc_grp_ctx;
+	struct axis2_qname *qname;
+
     conf_ctx = axis2_conf_ctx_create(&env, conf);
+	
+	svc_grp = axis2_svc_grp_create(&env);
+	svc_grp_ctx = axis2_svc_grp_ctx_create(&env, svc_grp, conf_ctx);
+	
+	qname = axis2_qname_create(&env, "name1", NULL, NULL);
+	svc = axis2_svc_create_with_qname(&env, qname);
+	svc_ctx = axis2_svc_ctx_create(&env, svc, svc_grp_ctx);
+
+	op  = axis2_op_create(&env);
+	op_ctx = axis2_op_ctx_create(&env, op, svc_ctx);
 
 	msg_ctx = axis2_msg_ctx_create(&env, conf_ctx, NULL, NULL);	
 
-	axis2_engine_t *engine = axis2_engine_create(&env, conf_ctx);
+    AXIS2_MSG_CTX_SET_CONF_CTX(msg_ctx, &env, conf_ctx);
+	AXIS2_MSG_CTX_SET_OP_CTX(msg_ctx, &env, op_ctx);
+	AXIS2_MSG_CTX_SET_SVC_CTX(msg_ctx, &env, svc_ctx);
 
+	axis2_engine_t *engine = axis2_engine_create(&env, conf_ctx);
     status = AXIS2_ENGINE_SEND(engine, &env, msg_ctx);
 	if(status != AXIS2_SUCCESS )
 	{
