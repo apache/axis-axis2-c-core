18797debca06203190a396eef507204c6f6eb073
Damitha N.M. Kumarage <damitha@apache.org>
2006-01-10 06:09:40 +0000

    warnings removed

--- test/core/engine/test_engine.c
+++ test/core/engine/test_engine.c
@@ -8,15 +8,12 @@ int	axis2_test_engine_send()
 	axis2_status_t status = AXIS2_FAILURE;
 	axis2_allocator_t *allocator = axis2_allocator_init (NULL);
 	axis2_env_t *env = axis2_env_create (allocator);
-	struct axis2_msg *msg = NULL;
     struct axis2_conf *conf = NULL;
 	conf = axis2_conf_create(&env);
 
     struct axis2_conf_ctx *conf_ctx;
 	struct axis2_msg_ctx *msg_ctx;
     conf_ctx = axis2_conf_ctx_create(&env, conf);
-	axis2_transport_in_desc_t *in_desc = NULL;
-	axis2_transport_out_desc_t *out_desc = NULL;
 
 	msg_ctx = axis2_msg_ctx_create(&env, conf_ctx, NULL, NULL);	
 
@@ -38,15 +35,12 @@ int	axis2_test_engine_receive()
 	axis2_status_t status = AXIS2_FAILURE;
 	axis2_allocator_t *allocator = axis2_allocator_init (NULL);
 	axis2_env_t *env = axis2_env_create (allocator);
-	struct axis2_msg *msg = NULL;
     struct axis2_conf *conf = NULL;
 	conf = axis2_conf_create(&env);
 
     struct axis2_conf_ctx *conf_ctx;
 	struct axis2_msg_ctx *msg_ctx;
     conf_ctx = axis2_conf_ctx_create(&env, conf);
-	axis2_transport_in_desc_t *in_desc = NULL;
-	axis2_transport_out_desc_t *out_desc = NULL;
 
 	msg_ctx = axis2_msg_ctx_create(&env, conf_ctx, NULL, NULL);	
 
