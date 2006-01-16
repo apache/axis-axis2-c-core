2f29a1f5085fa9dc9c6ea4001937d21b8e23e3e7
Don Samisa Abeysinghe <samisa@apache.org>
2006-01-16 03:39:50 +0000

    Fixed the build script to use deploy as home

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ./autogen.sh
-#AXIS2C_HOME=`pwd`
+AXIS2C_HOME=`pwd`/deploy
 ./configure --prefix=${AXIS2C_HOME}
 make
 make install
-cp install/lib/libaxis2_libxml2.so install/lib/libaxis2_parser.so
+cp deploy/lib/libaxis2_libxml2.so deploy/lib/libaxis2_parser.so
