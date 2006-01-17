89eb73367eb5c27f2a64bc812060a1b9f3e63f22
Don Samisa Abeysinghe <samisa@apache.org>
2006-01-17 09:01:42 +0000

    Changed to return axis2_transport_sender

--- include/axis2_http_transport_sender.h
+++ include/axis2_http_transport_sender.h
@@ -29,6 +29,7 @@
 #include <axis2_msg_ctx.h>
 #include <axis2_conf_ctx.h>
 #include <axis2_transport_out_desc.h>
+#include <axis2_transport_sender.h>
 
 #ifdef __cplusplus
 extern "C"
@@ -40,64 +41,10 @@ extern "C"
  * @{
  */
 
-    typedef struct axis2_http_transport_sender_ops 
-							axis2_http_transport_sender_ops_t;
-    typedef struct axis2_http_transport_sender axis2_http_transport_sender_t;
 
-    
-/** 
- * @brief HTTP Transport Sender ops struct
- * Encapsulator struct for ops of axis2_http_transport_sender
- */  
-AXIS2_DECLARE_DATA struct axis2_http_transport_sender_ops
-{
-    axis2_status_t (AXIS2_CALL *invoke) 
-							(axis2_http_transport_sender_t *transport_sender, 
-                    		axis2_env_t **env, axis2_msg_ctx_t *msg_ctx);
-    
-    axis2_status_t (AXIS2_CALL *clean_up) 
-							(axis2_http_transport_sender_t *transport_sender, 
-                    		axis2_env_t **env, axis2_msg_ctx_t *msg_ctx);
-    
-    axis2_status_t (AXIS2_CALL *init) 
-							(axis2_http_transport_sender_t *transport_sender, 
-                    		axis2_env_t **env, axis2_conf_ctx_t *conf_ctx, 
-							axis2_transport_out_desc_t *out_desc);
-    
-    axis2_status_t (AXIS2_CALL *free) 
-							(axis2_http_transport_sender_t *transport_sender, 
-                    		axis2_env_t **env);
-};
-
-/** 
- * @brief HTTP Transport Sender
- *    Axis2 HTTP Transport Sender
- */
-AXIS2_DECLARE_DATA struct axis2_http_transport_sender
-{
-    axis2_http_transport_sender_ops_t *ops;    
-};
-
-
-AXIS2_DECLARE(axis2_http_transport_sender_t *) 
+AXIS2_DECLARE(axis2_transport_sender_t *) 
 axis2_http_transport_sender_create (axis2_env_t **env);
 
-/************************** Start of function macros **************************/
-
-#define AXIS2_HTTP_TRANSPORT_SENDER_INVOKE(transport_sender, env, msg_ctx) \
-								((transport_sender)->ops->invoke(\
-								transport_sender, env, msg_ctx))
-#define AXIS2_HTTP_TRANSPORT_SENDER_CLEAN_UP(transport_sender, env,msg_ctx) \
-                                ((transport_sender)->ops->clean_up(\
-								transport_sender, env, msg_ctx))
-#define AXIS2_HTTP_TRANSPORT_SENDER_INIT(transport_sender, env, conf_ctx, \
-								out_desc) ((transport_sender)->ops->init(\
-								transport_sender, env, conf_ctx, out_desc))
-#define AXIS2_HTTP_TRANSPORT_SENDER_FREE(transport_sender, env) \
-                                ((transport_sender)->ops->free(\
-								transport_sender, env))
-
-/************************** End of function macros ****************************/    
 
 /** @} */
 #ifdef __cplusplus
