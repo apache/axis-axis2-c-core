b546bed01d952aca3e0dae512a68c6aa12afbde8
Damitha N.M. Kumarage <damitha@apache.org>
2006-01-24 02:55:40 +0000

    created a test to test wsdl_component

--- modules/wsdl/wsdl_component.c
+++ modules/wsdl/wsdl_component.c
@@ -107,22 +107,16 @@ axis2_wsdl_component_create (axis2_env_t **env)
 	}
     
 	wsdl_component_impl->wsdl_component.ops->free = axis2_wsdl_component_free;
-    
 	wsdl_component_impl->wsdl_component.ops->set_component_properties =
-        axis2_wsdl_component_set_component_properties;
-    
+            axis2_wsdl_component_set_component_properties;
     wsdl_component_impl->wsdl_component.ops->get_component_properties =
-        axis2_wsdl_component_get_component_properties;
-    
+            axis2_wsdl_component_get_component_properties;
     wsdl_component_impl->wsdl_component.ops->set_component_property =
-        axis2_wsdl_component_set_component_property;
-    
+            axis2_wsdl_component_set_component_property;
     wsdl_component_impl->wsdl_component.ops->get_component_property =
-        axis2_wsdl_component_get_component_property;
-    
+            axis2_wsdl_component_get_component_property;
     wsdl_component_impl->wsdl_component.ops->add_extensibility_element = 
             axis2_wsdl_component_add_extensibility_element;
-    
     wsdl_component_impl->wsdl_component.ops->get_extensibility_elements = 
             axis2_wsdl_component_get_extensibility_elements;
 	
@@ -169,7 +163,6 @@ axis2_wsdl_component_free (axis2_wsdl_component_t *wsdl_component,
             
             if(0 == AXIS2_STRCMP(AXIS2_MODULEREF_KEY, (axis2_char_t *)key))
             {
-                void *val = NULL;
                 int i = 0;
                 int size = 0;
                 module_desc_list = (axis2_array_list_t *) val;
@@ -178,21 +171,20 @@ axis2_wsdl_component_free (axis2_wsdl_component_t *wsdl_component,
                     size = AXIS2_ARRAY_LIST_SIZE(module_desc_list, env);
                     for (i = 0; i < size; i++)
                     {
-                        struct axis2_module_desc *module_desc = NULL;
-                        module_desc = AXIS2_ARRAY_LIST_GET(module_desc_list, env, i);
+                        axis2_module_desc_t *module_desc = NULL;
                         
-                        module_desc = (struct axis2_module_desc *) val;
+                        module_desc = AXIS2_ARRAY_LIST_GET(module_desc_list, 
+                            env, i);
                         if (module_desc)
-                           AXIS2_MODULE_DESC_FREE (module_desc, env);
-                        
-                        val = NULL;
-                        module_desc = NULL;
+                        {
+                            AXIS2_MODULE_DESC_FREE (module_desc, env);
+                            module_desc = NULL;
+                        }
                            
                     }
                     AXIS2_ARRAY_LIST_FREE(module_desc_list, env);
+                    module_desc_list = NULL;
                 }
-                
-                module_desc_list = NULL;
             }
             
             val = NULL;
@@ -318,7 +310,7 @@ axis2_wsdl_component_set_component_property(
 	return AXIS2_SUCCESS;
 }
 
-void * AXIS2_CALL
+void *AXIS2_CALL
 axis2_wsdl_component_get_component_property(
 		                                axis2_wsdl_component_t *wsdl_component,
                                         axis2_env_t **env, 
--- test/unit/wsdl/test_wsdl_component.c
+++ test/unit/wsdl/test_wsdl_component.c
@@ -1,10 +1,68 @@
 #include "test_wsdl_component.h"
 #include <axis2_string.h>
+#include <axis2_module_desc.h>
+#include <axis2_hash.h>
+#include <axis2_array_list.h>
+
+axis2_array_list_t *
+get_module_list(axis2_env_t **env)
+{
+    axis2_array_list_t *module_list = NULL;
+    axis2_module_desc_t *module_desc = NULL;
+    
+    module_list = axis2_array_list_create(env, 0);
+    if(!module_list)
+    {
+        return NULL;
+    }
+    module_desc = axis2_module_desc_create(env);
+    AXIS2_ARRAY_LIST_ADD(module_list, env, module_desc);
+    module_desc = NULL;
+    module_desc = axis2_module_desc_create(env);
+    AXIS2_ARRAY_LIST_ADD(module_list, env, module_desc);
+    module_desc = NULL;
+    module_desc = axis2_module_desc_create(env);
+    AXIS2_ARRAY_LIST_ADD(module_list, env, module_desc);
+    module_desc = NULL;
+
+    return module_list;
+}
+
+axis2_hash_t *
+get_module_list_map(axis2_env_t **env)
+{
+    axis2_hash_t *module_list_map = NULL;
+    axis2_array_list_t *module_list = NULL;
+    
+    module_list_map = axis2_hash_make(env);
+    if(!module_list_map)
+    {
+        return NULL;
+    }
+    module_list = get_module_list(env);
+    axis2_hash_set(module_list_map, AXIS2_MODULEREF_KEY, 
+        AXIS2_HASH_KEY_STRING, module_list);
+    module_list = NULL;
+
+    module_list = get_module_list(env);
+    axis2_hash_set(module_list_map, AXIS2_MODULEREF_KEY, 
+        AXIS2_HASH_KEY_STRING, module_list);
+    module_list = NULL;
+    
+    module_list = get_module_list(env);
+    axis2_hash_set(module_list_map, AXIS2_MODULEREF_KEY, 
+        AXIS2_HASH_KEY_STRING, module_list);
+    module_list = NULL;
+
+    return module_list_map;
+}
 
 void Testaxis2_wsdl_component_set_component_properties(CuTest *tc)
 {
-    axis2_char_t *expected = NULL;
-    axis2_char_t *actual = NULL;
+    axis2_status_t expected = AXIS2_SUCCESS;
+    axis2_status_t actual = AXIS2_FAILURE;
+    axis2_hash_t *module_list_map = NULL;
+    axis2_wsdl_component_t *wsdl_comp = NULL;
     
     printf("**************************************************************\n");
     printf("testing axis2_wsdl_component_set_component_properties  method \n");
@@ -13,5 +71,19 @@ void Testaxis2_wsdl_component_set_component_properties(CuTest *tc)
     axis2_allocator_t *allocator = axis2_allocator_init (NULL);
     axis2_env_t *env = axis2_env_create(allocator);
 
-    CuAssertStrEquals(tc, expected, actual);
+    wsdl_comp = axis2_wsdl_component_create(&env);
+    CuAssertPtrNotNull(tc, wsdl_comp);
+    module_list_map = get_module_list_map(&env);
+    CuAssertPtrNotNull(tc, module_list_map);
+    actual = AXIS2_WSDL_COMPONENT_SET_COMPONENT_PROPERTIES(wsdl_comp, &env, module_list_map);
+    /* again create a module_list_map and assign to see whether it works correctly */
+    module_list_map = NULL;
+    module_list_map = get_module_list_map(&env);
+    CuAssertPtrNotNull(tc, module_list_map);
+    actual = AXIS2_WSDL_COMPONENT_SET_COMPONENT_PROPERTIES(wsdl_comp, &env, module_list_map);
+    AXIS2_WSDL_COMPONENT_FREE(wsdl_comp, &env);
+   
+    CuAssertIntEquals(tc, expected, actual);
 }
+
+
