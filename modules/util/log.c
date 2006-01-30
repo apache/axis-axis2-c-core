22689b881220a50ed5da6821528d15f50a16a8dd
Damitha N.M. Kumarage <damitha@apache.org>
2006-01-30 10:05:08 +0000

    updated log.c to add more functions

--- modules/util/log.c
+++ modules/util/log.c
@@ -31,7 +31,18 @@ axis2_log_impl_free (axis2_log_t *log)
     return 0;
 }
 
-axis2_status_t AXIS2_CALL axis2_log_impl_write (struct axis2_log *log, const axis2_char_t *buffer, axis2_log_levels_t level);
+/* function prototypes */
+axis2_status_t AXIS2_CALL axis2_log_impl_write (axis2_log_t *log, const axis2_char_t *buffer, axis2_log_levels_t level,const axis2_char_t *file,const int line);
+axis2_status_t AXIS2_CALL axis2_log_impl_write_to_file(FILE *fd,axis2_log_levels_t level,const axis2_char_t *file,const int line,const axis2_char_t *value);
+axis2_status_t AXIS2_CALL axis2_log_impl_log_critical(axis2_log_t *log,const axis2_char_t *filename,const int linenumber,const axis2_char_t *format,...);
+
+axis2_status_t AXIS2_CALL axis2_log_impl_log_error(axis2_log_t *log,const axis2_char_t *filename,const int linenumber,const axis2_char_t *format,...);
+
+axis2_status_t AXIS2_CALL axis2_log_impl_log_warning(axis2_log_t *log,const axis2_char_t *filename,const int linenumber,const axis2_char_t *format,...);
+
+axis2_status_t AXIS2_CALL axis2_log_impl_log_info(axis2_log_t *log,const axis2_char_t *filename,const int linenumber,const axis2_char_t *format,...);
+
+axis2_status_t AXIS2_CALL axis2_log_impl_log_debug(axis2_log_t *log,const axis2_char_t *filename,const int linenumber,const axis2_char_t *format,...);
 
 AXIS2_DECLARE(axis2_log_t *)
 axis2_log_create (axis2_allocator_t * allocator, axis2_log_ops_t * ops)
@@ -60,13 +71,19 @@ axis2_log_create (axis2_allocator_t * allocator, axis2_log_ops_t * ops)
         }
         log->ops->free = axis2_log_impl_free;
         log->ops->write = axis2_log_impl_write;
+		log->ops->log_critical = axis2_log_impl_log_critical;
+		log->ops->log_error = axis2_log_impl_log_error;
+		log->ops->log_warning = axis2_log_impl_log_warning;
+		log->ops->log_info = axis2_log_impl_log_info;
+		log->ops->log_debug = axis2_log_impl_log_debug;
+
     }
 
     return log;
 }
-
+/*TODO:remove this method*/
 axis2_status_t AXIS2_CALL
-axis2_log_impl_write (struct axis2_log *log, const axis2_char_t *buffer, axis2_log_levels_t level)
+axis2_log_impl_write (axis2_log_t *log, const axis2_char_t *buffer, axis2_log_levels_t level,const axis2_char_t *file,const int line)
 {
     if (!log || !buffer)
         return -1;
@@ -91,10 +108,125 @@ axis2_log_impl_write (struct axis2_log *log, const axis2_char_t *buffer, axis2_l
                 level_str = "[debug] ";
                 break;
         }
-        fprintf (stderr, "%s", level_str);
-        fprintf (stderr, "%s\n", buffer);
+	  fprintf(stderr,"%s %s(%d) %s\n", level_str,file,line,buffer);
     }
     
     return 0;
+}
+
+
+axis2_status_t AXIS2_CALL axis2_log_impl_write_to_file(FILE *fd,axis2_log_levels_t level,const axis2_char_t *file,const int line,const axis2_char_t *value)
+{
+	if (!fd)
+		return -1;
+	/**
+      * print all critical and error logs irrespective of log->level setting
+	  */
+    char *level_str = "";
+    switch (level)
+    {
+    	case AXIS2_LOG_LEVEL_CRITICAL:
+        	level_str = "[critical] ";
+            break;
+        case AXIS2_LOG_LEVEL_ERROR:
+            level_str = "[error] ";
+            break;
+        case AXIS2_LOG_LEVEL_WARNING:
+            level_str = "[warning] ";
+            break;
+        case AXIS2_LOG_LEVEL_INFO:
+            level_str = "[info] ";
+            break;
+        case AXIS2_LOG_LEVEL_DEBUG:
+            level_str = "[debug] ";
+            break;
+    }
+	fprintf(fd,"%s %s(%d) %s\n", level_str,file,line,value);
+	return 0;
+}
+
+axis2_status_t AXIS2_CALL 
+axis2_log_impl_log_debug(axis2_log_t *log,const axis2_char_t *filename,const int linenumber,const axis2_char_t *format,...)
+{
+	if (!log || !format)
+		return -1;
+	
+	if (AXIS2_LOG_LEVEL_DEBUG <= log->level)
+	{
+		char value[AXIS2_LEN_VALUE+1];
+    	va_list ap;
+    	va_start(ap, format);
+    	vsnprintf(value, AXIS2_LEN_VALUE, format, ap);
+    	va_end(ap);
+		axis2_log_impl_write_to_file(stderr,AXIS2_LOG_LEVEL_DEBUG,filename,linenumber,value);
+	}
+	return 0;
+}
+
+axis2_status_t AXIS2_CALL 
+axis2_log_impl_log_info(axis2_log_t *log,const axis2_char_t *filename,const int linenumber,const axis2_char_t *format,...)
+{
+	if (!log || !format)
+		return -1;
+	
+	if (AXIS2_LOG_LEVEL_INFO <= log->level)
+	{
+		char value[AXIS2_LEN_VALUE+1];
+    	va_list ap;
+    	va_start(ap, format);
+    	vsnprintf(value, AXIS2_LEN_VALUE, format, ap);
+    	va_end(ap);
+		axis2_log_impl_write_to_file(stderr,AXIS2_LOG_LEVEL_INFO,filename,linenumber,value);
+	}
+	return 0;
+}
+
+axis2_status_t AXIS2_CALL 
+axis2_log_impl_log_warning(axis2_log_t *log,const axis2_char_t *filename,const int linenumber,const axis2_char_t *format,...)
+{
+	if (!log || !format)
+		return -1;
+	
+	if (AXIS2_LOG_LEVEL_WARNING <= log->level)
+	{
+		char value[AXIS2_LEN_VALUE+1];
+    	va_list ap;
+    	va_start(ap, format);
+    	vsnprintf(value, AXIS2_LEN_VALUE, format, ap);
+    	va_end(ap);
+		axis2_log_impl_write_to_file(stderr,AXIS2_LOG_LEVEL_WARNING,filename,linenumber,value);
+	}
+	return 0;
+}
+
+
+axis2_status_t AXIS2_CALL 
+axis2_log_impl_log_error(axis2_log_t *log,const axis2_char_t *filename,const int linenumber,const axis2_char_t *format,...)
+{
+	if (!log || !format)
+		return -1;
+	
+	char value[AXIS2_LEN_VALUE+1];
+   	va_list ap;
+   	va_start(ap, format);
+   	vsnprintf(value, AXIS2_LEN_VALUE, format, ap);
+   	va_end(ap);
+	axis2_log_impl_write_to_file(stderr,AXIS2_LOG_LEVEL_ERROR,filename,linenumber,value);
+	return 0;
+}
+	
+axis2_status_t AXIS2_CALL 
+axis2_log_impl_log_critical(axis2_log_t *log,const axis2_char_t *filename,const int linenumber,const axis2_char_t *format,...)
+{
+	if (!log || !format)
+		return -1;
+	
+	char value[AXIS2_LEN_VALUE+1];
+   	va_list ap;
+   	va_start(ap, format);
+   	vsnprintf(value, AXIS2_LEN_VALUE, format, ap);
+   	va_end(ap);
+	axis2_log_impl_write_to_file(stderr,AXIS2_LOG_LEVEL_CRITICAL,filename,linenumber,value);
+	return 0;
 }
 
