a711f86c7f3d496599b3ff02d6b81b75f361847a
Damitha N.M. Kumarage <damitha@apache.org>
2006-02-01 10:34:27 +0000

    Patch applied mentioned in mail [AXIS2]help needed on gnu build system. Thanks Sanjaya for the patch.

--- test/xml/soap/test_soap.c
+++ test/xml/soap/test_soap.c
@@ -247,6 +247,7 @@ int main(int argc, char *argv[])
     }
     allocator = axis2_allocator_init (NULL);
     log = axis2_log_create(allocator, NULL);
+    log->level = AXIS2_LOG_LEVEL_DEBUG;
     error = axis2_error_create(allocator);
     env = axis2_env_create_with_error_log(allocator, error,  log);
     
