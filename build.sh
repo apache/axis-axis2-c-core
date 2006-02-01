0168284b71182cd5fc5ab0edc660231d02eea99e
Damitha N.M. Kumarage <damitha@apache.org>
2006-02-01 11:00:56 +0000

    There was junk commited in previous commit. Removed it

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./autogen.sh
-AXIS2C_HOME=/home/sanjaya/tmp
-./configure --prefix=${AXIS2C_HOME} --enable-tests=yes --enable-samples=yes
+AXIS2C_HOME=`pwd`/deploy
+./configure --prefix=${AXIS2C_HOME}
 make
 make install
