7057cdfe9ef18bcb1d9b087fb71e7b52c9bd4cb0
Damitha N.M. Kumarage <damitha@apache.org>
2006-02-02 04:00:55 +0000

    enabled tests and samples in build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ./autogen.sh
 AXIS2C_HOME=`pwd`/deploy
-./configure --prefix=${AXIS2C_HOME}
+./configure --prefix=${AXIS2C_HOME} --enable-tests=yes --enable-samples=yes
 make
 make install
 cp -f ${AXIS2C_HOME}/lib/libecho.so ${AXIS2C_HOME}/services/echo
