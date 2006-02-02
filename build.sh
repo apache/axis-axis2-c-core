d892934a3f729914459bdcf8a939d31ceeba332f
Damitha N.M. Kumarage <damitha@apache.org>
2006-02-02 09:55:52 +0000

    Since sanjaya's last patch copying xml files is no longer needed

--- build.sh
+++ build.sh
@@ -4,6 +4,3 @@ AXIS2C_HOME=`pwd`/deploy
 ./configure --prefix=${AXIS2C_HOME} --enable-tests=yes --enable-samples=yes
 make
 make install
-cp -f samples/server/echo/services.xml ${AXIS2C_HOME}/services/echo
-cp -f samples/server/math/services.xml ${AXIS2C_HOME}/services/math
-cp -f samples/server/axis2.xml ${AXIS2C_HOME}
