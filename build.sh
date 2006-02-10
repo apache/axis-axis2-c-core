f17be09d3529115b0a34084e3395d5fe7c8fa073
Don Samisa Abeysinghe <samisa@apache.org>
2006-02-10 05:44:54 +0000

    now we have the configure option --enable-trace. by default this no. added --enable-trace=yes to build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./autogen.sh
 AXIS2C_HOME=`pwd`/deploy
-./configure --prefix=${AXIS2C_HOME} --enable-tests=yes --enable-samples=yes
+./configure --prefix=${AXIS2C_HOME} --enable-tests=yes --enable-samples=yes --enable-trace=yes
 make
 make install
