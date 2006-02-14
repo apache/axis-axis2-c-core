06070389f302d808306b1b3beae433b580573603
Damitha N.M. Kumarage <damitha@apache.org>
2006-02-14 11:10:10 +0000

    updated description test

--- test/core/description/test_description.c
+++ test/core/description/test_description.c
@@ -19,18 +19,16 @@ int axis2_test_op_engage_module()
 
     axis2_allocator_t *allocator = axis2_allocator_init (NULL);
 	axis2_env_t *env = axis2_env_create (allocator);
-	struct axis2_op *op = axis2_op_create(&env);
+	axis2_op_t *op = axis2_op_create(&env);
 
     moduleref = axis2_module_desc_create(&env);
 
     status = AXIS2_OP_ENGAGE_MODULE(op, &env, moduleref);
-
+    moduleref = NULL;
 	if(status != AXIS2_SUCCESS )
 	{
 	    AXIS2_OP_FREE(op, &env);
-	    AXIS2_MODULE_DESC_FREE(moduleref, &env);
 		printf("ERROR %d\n", status);
-	    /*return -1;*/
 	}
 
 	AXIS2_OP_FREE(op, &env);
@@ -76,10 +74,10 @@ int axis2_test_svc_add_module_ops()
 
 int axis2_test_svc_engage_module()
 {
-	struct axis2_svc *svc = NULL;
-	struct axis2_qname *qname = NULL;
-	struct axis2_module_desc *moduleref = NULL;
-	struct axis2_conf *axis2_config = NULL;	
+	axis2_svc_t *svc = NULL;
+	axis2_qname_t *qname = NULL;
+	axis2_module_desc_t *moduleref = NULL;
+	axis2_conf_t *axis2_config = NULL;	
 	axis2_status_t status = AXIS2_FAILURE;
 
     
@@ -95,7 +93,7 @@ int axis2_test_svc_engage_module()
 	axis2_config = axis2_conf_create(&env);
 	
 	status = AXIS2_SVC_ENGAGE_MODULE(svc, &env, moduleref, axis2_config); 
-
+    moduleref = NULL;
 	if(status != AXIS2_SUCCESS)
 	{
 	    printf("axis2_test_description_svc_engage_module ERROR %d\n", status);
@@ -105,7 +103,6 @@ int axis2_test_svc_engage_module()
 
 	AXIS2_SVC_FREE(svc, &env);
 	AXIS2_QNAME_FREE(qname, &env);
-	AXIS2_MODULE_DESC_FREE(moduleref, &env);
 	AXIS2_CONF_FREE(axis2_config, &env);
 
 	return 0;
