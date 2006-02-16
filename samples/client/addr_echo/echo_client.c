1b4faff6400987abd3606d9a97765e30017ee38f
Damitha N.M. Kumarage <damitha@apache.org>
2006-02-16 05:48:44 +0000

    changed the default logfile name

--- samples/client/addr_echo/echo_client.c
+++ samples/client/addr_echo/echo_client.c
@@ -64,7 +64,7 @@ int main(int argc, char** argv)
     
     allocator = axis2_allocator_init (NULL);
     error = axis2_error_create(allocator);
-    log = axis2_log_create(allocator, NULL, "/dev/stderr");
+    log = axis2_log_create(allocator, NULL, "addr_echo.log");
     env = axis2_env_create_with_error_log(allocator, error, log);
     env->log->level = AXIS2_LOG_LEVEL_TRACE;
     axis2_error_init();
