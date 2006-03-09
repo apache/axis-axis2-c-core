d799d564076b89ff0a365a1b616f32fc505833e5
Damitha N.M. Kumarage <damitha@apache.org>
2006-03-09 09:49:31 +0000

    Added with-apache2=PATH configure option

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./autogen.sh
 AXIS2C_HOME=`pwd`/deploy
-./configure --prefix=${AXIS2C_HOME} --enable-tests=yes --enable-samples=yes --enable-trace=yes
+./configure --prefix=${AXIS2C_HOME} --enable-tests=yes --enable-samples=yes --enable-trace=yes  --with-apache2=/usr/local/apache2
 make
 make install
