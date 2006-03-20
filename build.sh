332ebc85b5f6bd2ca6930f981750af473d3136d1
Damitha N.M. Kumarage <damitha@apache.org>
2006-03-20 05:53:46 +0000

    seperate build system for samples

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./autogen.sh
 AXIS2C_HOME=`pwd`/deploy
-./configure --prefix=${AXIS2C_HOME} --enable-tests=yes --enable-samples=yes --enable-trace=yes  --with-apache2=/usr/include/apache2 --with-apr=/usr/include/apr-0
+./configure --prefix=${AXIS2C_HOME} --enable-tests=yes --enable-trace=yes  --with-apache2=/usr/include/apache2 --with-apr=/usr/include/apr-0
 make
-#make install
+make install
