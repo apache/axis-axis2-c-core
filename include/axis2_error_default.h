715506a7f40e32d5e470680114ff0fad30861e19
Damitha N.M. Kumarage <damitha@apache.org>
2006-03-26 08:48:58 +0000

    moving axis2_linked_list.h, axis2_array_list.h, axis2_log_default.h axis2_error_default.h from modules/util to include.

---
+++ include/axis2_error_default.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2004,2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef AXIS2_ERROR_DEFAULT_H
+#define AXIS2_ERROR_DEFAULT_H
+
+#include <axis2_defines.h>
+#include <axis2_allocator.h>
+#include <axis2_error.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/**
+ * @defgroup axis2_error Error
+ * @ingroup axis2_util 
+ * @{
+ */
+
+
+  /**
+    * Creates an error struct
+    * @param allocator allocator to be used. Mandatory, cannot be NULL    
+    * @return pointer to the newly created error struct 
+    */
+    AXIS2_DECLARE(axis2_error_t *) axis2_error_create (axis2_allocator_t * allocator);
+
+
+/** @} */
+    
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif                          /* AXIS2_ERROR_DEFAULT_H */
