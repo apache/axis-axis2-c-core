b4db79fb2c2ac40073dff132a7cc537f01b37816
Damitha N.M. Kumarage <damitha@apache.org>
2006-03-28 10:05:38 +0000

    fixed a miner bug

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ./autogen.sh
 AXIS2C_HOME=`pwd`/deploy
+export AXIS2C_HOME
 ./configure --prefix=${AXIS2C_HOME} --enable-tests=yes --enable-trace=yes  --with-apache2=/usr/include/apache2 --with-apr=/usr/include/apr-0
 make
 make install
