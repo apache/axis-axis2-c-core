847a6d0b5d9b1edc39880b9790171e852233c6e8
Korale Gamaralalage Nandika Chandrasiri Jayawardana <nandika@apache.org>
2006-05-02 09:04:57 +0000

    error in include directive corrected

--- modules/xml/soap/soap_header.c
+++ modules/xml/soap/soap_header.c
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
  
-#include <axis2_soap_const.h>envelope.h"
+#include  "_axis2_soap_envelope.h"
  #include "_axis2_soap_header.h"
  #include <axis2_soap_header_block.h>
  #include <axis2_hash.h>
