4fe68c171e4ec5927daa9694e0009aabebd26c42
Damitha N.M. Kumarage <damitha@apache.org>
2006-05-18 10:50:17 +0000

    Changes due to folder structure change

--- samples/build.sh
+++ samples/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ./autogen.sh
-./configure --prefix=${AXIS2C_HOME} 
+./configure --prefix=${AXIS2C_HOME} --with-axis2_util=${AXIS2C_HOME}/include --with-axiom=${AXIS2C_HOME}/include
 make
 make install
