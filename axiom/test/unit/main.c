c5e10eb7ef2b6f80aa049869d3382d2f6337214c
Damitha N.M. Kumarage <damitha@apache.org>
2006-05-19 05:43:05 +0000

    Changes due to test moves into respective projects

---
+++ axiom/test/unit/main.c
@@ -0,0 +1,19 @@
+#include <CuTest.h>
+#include "om/om_test.h"
+
+void RunAllTests(void) {
+    CuString *output = CuStringNew();
+    CuSuite* suite = CuSuiteNew();
+
+    CuSuiteAddSuite(suite, axis2_omGetSuite());
+    
+    CuSuiteRun(suite);
+    CuSuiteSummary(suite, output);
+    CuSuiteDetails(suite, output);
+    printf("%s\n", output->buffer);
+}
+
+int main(void) {
+    RunAllTests();
+    return 0;
+}
