3597c64bfb28b765296e0ef84b5d686cecd02514
Damitha N.M. Kumarage <damitha@apache.org>
2006-05-19 06:57:31 +0000

    More changes due to test folder moves into respective projects

--- axiom/build.sh
+++ axiom/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ./autogen.sh
-./configure --prefix=${AXIS2C_HOME} --enable-static=no --with-axis2_util=${AXIS2C_HOME}/include
+./configure --prefix=${AXIS2C_HOME} --enable-static=no --with-axis2_util=${AXIS2C_HOME}/include --enable-tests=yes
 make
 make install
