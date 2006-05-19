b2c8bc60189943eb260c30ff14d8df6154febb1d
Damitha N.M. Kumarage <damitha@apache.org>
2006-05-19 09:39:45 +0000

    --enable-test=no by default

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 AXIS2C_HOME=`pwd`/deploy
 AXIS2C=`pwd`
 export AXIS2C_HOME AXIS2C
-./configure --prefix=${AXIS2C_HOME} --enable-tests=yes --enable-trace=yes  --with-apache2=/usr/include/apache2 --with-apr=/usr/include/apr-0 --enable-diclient=no --enable-static=no --enable-openssl=no --with-axis2_util=${AXIS2C}/util/include --with-axiom=${AXIS2C}/axiom/include
+./configure --prefix=${AXIS2C_HOME} --enable-tests=no --enable-trace=yes  --with-apache2=/usr/include/apache2 --with-apr=/usr/include/apr-0 --enable-diclient=no --enable-static=no --enable-openssl=no --with-axis2_util=${AXIS2C}/util/include --with-axiom=${AXIS2C}/axiom/include
 make
 make install
 cd samples
