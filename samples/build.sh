dd57b960eb5c677f2cc97fe40ef7e4831f3debfa
Sanjaya Ratnaweera <pini@apache.org>
2006-05-25 12:17:05 +0000

    added error handling to build.sh script

--- samples/build.sh
+++ samples/build.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
+
 ./autogen.sh
+
 ./configure --prefix=${AXIS2C_HOME} --with-axis2_util=${AXIS2C_HOME}/include --with-axiom=${AXIS2C_HOME}/include
-make
-make install
+if [ -s error.log ]
+then
+    echo "Error, see error.log for details"
+	exit 2
+fi
+
+make  2>error.log
+	if grep -w 'Error' `find -name "error.log"`; then
+	echo "Error, see error.log for details"
+	exit 2
+fi
+
+make install 2>error.log
+	if grep -w 'Error' `find -name "error.log"`; then
+	echo "Error, see error.log for details"
+	exit 2
+fi
+
