0d1014c54962a0a301bed91e52726b8e9cfb0031
Sanjaya Ratnaweera <pini@apache.org>
2006-05-25 13:06:59 +0000

    added error handling to build_all.sh script

--- build_all.sh
+++ build_all.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 cd util
-sh build.sh
+
+sh build.sh  2>error.log
+    if grep -w 'Error' `find -name "error.log"`; then
+	echo "Error, see util/error.log for details"
+	exit 2
+fi
+
 cd ..
+
 cd axiom
-sh build.sh
+
+sh build.sh  2>error.log
+    if grep -w 'Error' `find -name "error.log"`; then
+	echo "Error, see util/error.log for details"
+	exit 2
+fi
+
 cd ..
-./autogen.sh
-AXIS2C_HOME=`pwd`/deploy
-AXIS2C=`pwd`
-export AXIS2C_HOME AXIS2C
-./configure --prefix=${AXIS2C_HOME} --enable-tests=yes --enable-trace=yes  --with-apache2=/usr/include/apache2 --with-apr=/usr/include/apr-0 --enable-diclient=no --enable-static=no --enable-openssl=no --with-axis2_util=${AXIS2C}/util/include --with-axiom=${AXIS2C}/axiom/include
-make
-make install
+
+sh build.sh  2>error.log
+    if grep -w 'Error' `find -name "error.log"`; then
+	echo "Error, see util/error.log for details"
+	exit 2
+fi
+
 cd samples
-sh build.sh
+
+sh build.sh  2>error.log
+    if grep -w 'Error' `find -name "error.log"`; then
+	echo "Error, see util/error.log for details"
+	exit 2
+fi
+
 cd ..
