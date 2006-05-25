0a0b8b30559bdce901aeb257503b3b2655dd624f
Sanjaya Ratnaweera <pini@apache.org>
2006-05-25 10:47:03 +0000

    added error handling for buils.sh script.

--- util/build.sh
+++ util/build.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
+
 ./autogen.sh
-./configure --prefix=${AXIS2C_HOME} --enable-static=no --enable-tests=yes
-make
-make install
+
+./configure --prefix=${AXIS2C_HOME} --enable-static=no 2> error.log
+if [ -s error.log ]
+then
+	echo "Error, see error.log for details" 
+	exit 2
+fi
+
+make  2>error.log
+if grep -w 'Error' `find -name "error.log"`; then
+	echo "Error, see error.log for details"
+	exit 2
+fi
+
+
+make install 2>error.log
+if grep -w 'Error' `find -name "error.log"`; then
+	echo "Error, see error.log for details"
+	exit 2
+fi
