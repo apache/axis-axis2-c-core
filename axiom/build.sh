18b2a91a15dc6750e2ab68adbc1d917259af1831
Sanjaya Ratnaweera <pini@apache.org>
2006-05-25 12:20:05 +0000

    fixed a miner bug

--- axiom/build.sh
+++ axiom/build.sh
@@ -2,7 +2,7 @@
 
 ./autogen.sh
 
-./configure --prefix=${AXIS2C_HOME} --enable-static=no --with-axis2_util=${AXIS2C_HOME}/include --enable-tests=yes
+./configure --prefix=${AXIS2C_HOME} --enable-static=no --with-axis2_util=${AXIS2C_HOME}/include --enable-tests=yes 2> error.log
 if [ -s error.log ]
 then
     echo "Error, see error.log for details"
