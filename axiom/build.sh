b277313a2d6450b17274506d76244a59eb173a8d
Sanjaya Ratnaweera <pini@apache.org>
2006-05-25 12:11:26 +0000

    added error handling to build.sh script

--- axiom/build.sh
+++ axiom/build.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
+
 ./autogen.sh
+
 ./configure --prefix=${AXIS2C_HOME} --enable-static=no --with-axis2_util=${AXIS2C_HOME}/include --enable-tests=yes
-make
-make install
+if [ -s error.log ]
+then
+    echo "Error, see error.log for details"
+	exit 2
+fi
+
+make  2>error.log
+if grep -w 'Error' `find -name "error.log"`; then
+	echo "Error, see error.log for details"
+	exit 2
+fi
+
+make install 2>error.log
+if grep -w 'Error' `find -name "error.log"`; then
+	echo "Error, see error.log for details"
+	exit 2
+fi
+
