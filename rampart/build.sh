83acee693104afb182d3a75643e5e04bc532857b
Sanjaya Ratnaweera <pini@apache.org>
2006-06-15 04:28:09 +0000

    fixed a minor bug

--- rampart/build.sh
+++ rampart/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./autogen.sh
 RAMPART_HOME=`pwd`/target
-./configure --prefix=${RAMPART_HOME} --enable-static=no --with-axis2_util=${AXIS2C_HOME}/include --with-axis2_axiom=${AXIS2C_HOME}/include --with-axis2_xml_schema=/home/kau/kauWS/c/xml_schema/include
+./configure --prefix=${RAMPART_HOME} --enable-static=no --with-axis2_util=${AXIS2C_HOME}/include --with-axis2_axiom=${AXIS2C_HOME}/include --with-axis2_xml_schema=${AXIS2C_HOME}/include
 make
 make install
