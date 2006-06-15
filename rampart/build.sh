ae6766d7a9f5b852709017ca6e19fd159befa736
Sanjaya Ratnaweera <pini@apache.org>
2006-06-15 08:37:45 +0000

    installing rampart to correct location

--- rampart/build.sh
+++ rampart/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 ./autogen.sh
-RAMPART_HOME=`pwd`/target
-./configure --prefix=${RAMPART_HOME} --enable-static=no --with-axis2_util=${AXIS2C_HOME}/include --with-axis2_axiom=${AXIS2C_HOME}/include --with-axis2_xml_schema=${AXIS2C_HOME}/include
+./configure --prefix=${AXIS2C_HOME}/rampart --enable-static=no --with-axis2_util=${AXIS2C_HOME}/include --with-axis2_axiom=${AXIS2C_HOME}/include --with-axis2_xml_schema=${AXIS2C_HOME}/include
 make
 make install
