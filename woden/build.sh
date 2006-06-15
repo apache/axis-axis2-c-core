69c2b0e37764e50a3b60ba05fdc7d6cf4e976ca4
Don Samisa Abeysinghe <samisa@apache.org>
2006-06-15 06:05:41 +0000

    Added make install

--- woden/build.sh
+++ woden/build.sh
@@ -4,3 +4,4 @@ AXIS2C=`pwd`/..
 
 ./configure --prefix=${AXIS2C_HOME} --enable-static=no --with-axis2_util=${AXIS2C_HOME}/include --with-axis2_axiom=${AXIS2C_HOME}/include --with-axis2_xml_schema=${AXIS2C}/xml_schema/include
 make
+make install
