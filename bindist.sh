0aea8b051bc81f596d400195852fcad62beca14b
Sanjaya Ratnaweera <pini@apache.org>
2006-06-15 10:17:25 +0000

    corrections to 'make bindist'

--- bindist.sh
+++ bindist.sh
@@ -1,3 +1,16 @@
+cd util
+sh build.sh
+cd ../axiom
+sh build.sh
+cd ../xml_schema
+sh build.sh
+cd ..
+sh build.sh
+cd rampart
+sh build.sh
+cd ../woden
+sh build.sh
+cd ..
 rm -rf xdocs/api/html
 maven site
 cd xdocs/api 
@@ -5,3 +18,15 @@ doxygen doxygenconf
 cd ../..
 cp -r xdocs/api/html target/docs/api/
 cp -r target/docs ${AXIS2C_HOME}
+
+cd ${AXIS2C_HOME}
+for i in `find . -name "*.la"`
+do
+   rm $i
+done
+
+for i in `find . -name "*.a"`
+do
+   rm $i
+done
+
