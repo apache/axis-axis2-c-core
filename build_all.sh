624a25ea3ff05ce58506cf8adbffb95081c97f64
Sanjaya Ratnaweera <pini@apache.org>
2006-07-07 10:16:56 +0000

    added woden to build_all.sh script

--- build_all.sh
+++ build_all.sh
@@ -4,25 +4,7 @@ export AXIS2C_HOME AXIS2C
 
 cd util
 
-./autogen.sh
-if [ "$?" -ne 0 ]
-then
-exit 1
-fi
-
-./configure --prefix=${AXIS2C_HOME} --enable-static=no
-if [ "$?" -ne 0 ]
-then
-exit 1
-fi
-
-make
-if [ "$?" -ne 0 ]
-then
-exit 1
-fi
-
-make install
+sh build.sh
 if [ "$?" -ne 0 ]
 then
 exit 1
@@ -30,25 +12,7 @@ fi
 
 cd ../axiom
 
-./autogen.sh
-if [ "$?" -ne 0 ]
-then
-exit 1
-fi
-
-./configure --prefix=${AXIS2C_HOME} --enable-static=no --with-axis2_util=${AXIS2C_HOME}/include --enable-tests=yes
-if [ "$?" -ne 0 ]
-then
-exit 1
-fi
-
-make
-if [ "$?" -ne 0 ]
-then
-exit 1
-fi
-
-make install
+sh build.sh
 if [ "$?" -ne 0 ]
 then
 exit 1
@@ -56,51 +20,21 @@ fi
 
 cd ../xml_schema
 
-./autogen.sh
+sh build.sh
 if [ "$?" -ne 0 ]
 then
 exit 1
 fi
 
-./configure --prefix=${AXIS2C_HOME} --enable-static=no --with-axis2_util=${AXIS2C_HOME}/include --with-axiom=${AXIS2C_HOME}/include
+cd ../woden
+sh build.sh
 if [ "$?" -ne 0 ]
 then
 exit 1
 fi
 
-make
-if [ "$?" -ne 0 ]
-then
-exit 1
-fi
-
-make install
-if [ "$?" -ne 0 ]
-then
-exit 1
-fi
-
-cd ..
-
-./autogen.sh
-if [ "$?" -ne 0 ]
-then
-exit 1
-fi
-
-./configure --prefix=${AXIS2C_HOME} --enable-tests=yes --enable-trace=yes  --with-apache2=/usr/include/apache2 --with-apr=/usr/include/apr-0 --enable-diclient=no --enable-static=no --enable-openssl=no --with-axis2_util=${AXIS2C}/util/include --with-axiom=${AXIS2C}/axiom/include
-if [ "$?" -ne 0 ]
-then
-exit 1
-fi
-
-make
-if [ "$?" -ne 0 ]
-then
-exit 1
-fi
-
-make install
+cd ../
+sh build.sh
 if [ "$?" -ne 0 ]
 then
 exit 1
@@ -108,28 +42,10 @@ fi
 
 cd samples
 
-./autogen.sh
+sh build.sh
 if [ "$?" -ne 0 ]
 then
 exit 1
-fi
-
-./configure --prefix=${AXIS2C_HOME} --with-axis2_util=${AXIS2C_HOME}/include --with-axiom=${AXIS2C_HOME}/include
-if [ "$?" -ne 0 ]
-then
-exit 1
-fi
-
-make
-if [ "$?" -ne 0 ]
-then
-exit 1
-fi
-
-make install
-if [ "$?" -ne 0 ]
-then
-exit
 fi
 
 cd ..
