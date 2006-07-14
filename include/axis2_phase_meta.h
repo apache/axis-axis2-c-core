60c7c6349651d60a66f59ada8a03752edd6518c9
Sanjaya Ratnaweera <pini@apache.org>
2006-07-14 08:46:33 +0000

    Updated doxygen comments to clean doc structure

--- include/axis2_phase_meta.h
+++ include/axis2_phase_meta.h
@@ -1,21 +1,27 @@
 /*
- * Copyright 2004,2005 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Copyright 2004,2005 The Apache Software Foundation.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 #ifndef AXIS2_PHASE_META_H
 #define AXIS2_PHASE_META_H
+
+/**
+ * @defgroup axis2_phase_meta phase meta data
+ * @ingroup axis2_engine
+ * @{
+ */
 
 /**
  * @file axis2_conf.h
@@ -29,37 +35,42 @@ extern "C"
 {
 #endif
 
+/** Axis2 in flow */
+#define  AXIS2_INFLOW  1
+/** Axis2 out flow */
+#define  AXIS2_OUTFLOW 2
+/** Axis2 fault in flow */
+#define  AXIS2_FAULT_INFLOW 3
+/** Axis2 fault out flow */
+#define  AXIS2_FAULT_OUTFLOW 4
+
+/** phase transport in */
+#define AXIS2_PHASE_TRANSPORTIN "TransportIn"
+/** phase pre dispatch */
+#define AXIS2_PHASE_PRE_DISPATCH "PreDispatch"
+/** phase dispatch */
+#define AXIS2_PHASE_DISPATCH "Dispatch"
+/** phase post dispatch */
+#define AXIS2_PHASE_POST_DISPATCH "PostDispatch"
+/** phase policy determination */
+#define AXIS2_PHASE_POLICY_DETERMINATION "PolicyDetermination"
+/** phase message processing */
+#define AXIS2_PHASE_MESSAGE_PROCESSING "MessageProcessing"
+
+/** phase message out */
+#define AXIS2_PHASE_MESSAGE_OUT "MessageOut"
+/** phase transport out */
+#define AXIS2_PHASE_TRANSPORT_OUT "MessageOut"
+
 /**
- * @defgroup axis2_phase_meta phase meta data
- * @ingroup axis2_engine
- * @{
- */
-     #define  AXIS2_INFLOW  1
-     #define  AXIS2_OUTFLOW 2
-     #define  AXIS2_FAULT_INFLOW 3
-     #define  AXIS2_FAULT_OUTFLOW 4
-
-    /* INFLOW */
-     #define AXIS2_PHASE_TRANSPORTIN "TransportIn"
-     #define AXIS2_PHASE_PRE_DISPATCH "PreDispatch"
-     #define AXIS2_PHASE_DISPATCH "Dispatch"
-     #define AXIS2_PHASE_POST_DISPATCH "PostDispatch"
-     #define AXIS2_PHASE_POLICY_DETERMINATION "PolicyDetermination"
-     #define AXIS2_PHASE_MESSAGE_PROCESSING "MessageProcessing"
-
-    /* OUTFLOW */
-     #define AXIS2_PHASE_MESSAGE_OUT "MessageOut"
-     #define AXIS2_PHASE_TRANSPORT_OUT "MessageOut"
-     
-    /**
-     * All the handlers inside transport_sender and tranport_recievre in axis2.xml gose
-     * to this phase
-     */
-     #define AXIS2_TRANSPORT_PHASE "TRANSPORT"
+ * All the handlers inside transport_sender and tranport_recievre in axis2.xml gose
+ * to this phase
+*/
+#define AXIS2_TRANSPORT_PHASE "TRANSPORT"
 
 
 /** @} */
-    
+
 #ifdef __cplusplus
 }
 #endif
