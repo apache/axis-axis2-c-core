9329ce843fb50514d2b2c18cb302977ae4b2652f
Sanjaya Ratnaweera <pini@apache.org>
2006-07-28 04:30:51 +0000

    Updated doxygen comments to clean doc structure

--- include/axis2_any_content_type.h
+++ include/axis2_any_content_type.h
@@ -18,6 +18,13 @@
 #define AXIS2_ANY_CONTENT_TYPE_H
 
 /**
+ * @defgroup axis2_any_content_type any content type
+ * @ingroup axis2_addr
+ * @{
+ */
+
+
+/**
  * @file axis2_any_content_type.h
  * @brief Axis2 core addressing interface
  */
@@ -33,103 +40,116 @@ extern "C"
 {
 #endif
 
+    /** Type name for axis2_any_content_type */
     struct axis2_any_content_type;
+    /** Type name for axis2_any_content_type_ops */
     struct axis2_any_content_type_ops;
-    
-/**
- * @defgroup axis2_any_content_type any content type
- * @ingroup axis2_addr
- * @{
- */
 
-/**
- *   \brief any_content_type ops struct
- */
- AXIS2_DECLARE_DATA typedef struct axis2_any_content_type_ops
-    { 
+    /**
+     * any_content_type ops struct
+     */
+    AXIS2_DECLARE_DATA typedef struct axis2_any_content_type_ops
+    {
         /**
          * Adds given value to the content value map.
          * @param any_content_type any_content_type struct. cannot be NULL.
-         * @param env Environment. MUST NOT be NULL.
+         * @param env pointer to environment struct. MUST NOT be NULL.
+	 * @param qname pointer to qname
          * @param value value to be added.
          * @return AXIS2_SUCCESS on success else AXIS2_FAILURE
          */
         axis2_status_t (AXIS2_CALL *
-        add_value)(struct axis2_any_content_type *any_content_type,
-                   const axis2_env_t *env, 
-                   axis2_qname_t *qname, 
-                   axis2_char_t *value); 
-       
+                add_value)(
+                    struct axis2_any_content_type *any_content_type,
+                    const axis2_env_t *env,
+                    axis2_qname_t *qname,
+                    axis2_char_t *value);
+
         /**
          * Gets the value from the content value map.
          * @param any_content_type any_content_type struct. cannot be NULL.
-         * @param env Environment. MUST NOT be NULL.
+         * @param env pointer to environment struct. MUST NOT be NULL.
          * @param qname qname of the corresponging value to be retrived
          * @return Pointer to the value if present, else returns NULL. 
-         *          Environment status would be set to AXIS2_FAILURE on error.
+         * Environment status would be set to AXIS2_FAILURE on error.
          */
         axis2_char_t* (AXIS2_CALL *
-      get_value)(struct axis2_any_content_type *any_content_type, 
-               const axis2_env_t *env, 
-               axis2_qname_t *qname);
+                get_value)(
+                    struct axis2_any_content_type *any_content_type,
+                    const axis2_env_t *env,
+                    axis2_qname_t *qname);
         /**
          * Gets the map of values
          * @param any_content_type any_content_type struct. cannot be NULL.
-         * @param env Environment. MUST NOT be NULL.
+         * @param env pointer to environment struct. MUST NOT be NULL.
          * @return Pointer to the value map. Would return NULL and set 
-         *          Environment status to AXIS2_FAILURE on error.
+         * Environment status to AXIS2_FAILURE on error.
          */
         axis2_hash_t* (AXIS2_CALL *
-      get_value_map)(struct axis2_any_content_type *any_content_type, 
-                  const axis2_env_t *env);
-      
+                get_value_map)(
+                    struct axis2_any_content_type *any_content_type,
+                    const axis2_env_t *env);
+
         /**
          * Frees the given any_content_type struct
          * @param any_content_type any_content_type struct. cannot be NULL.
-         * @param env Environment. MUST NOT be NULL.
+         * @param env pointer to environment struct. MUST NOT be NULL.
          * @return AXIS2_SUCCESS on success else AXIS2_FAILURE
          */
         axis2_status_t (AXIS2_CALL *
-      free)(struct axis2_any_content_type *any_content_type, 
-              const axis2_env_t *env);
-      
-    } axis2_any_content_type_ops_t;
-   
-   /** 
-    * \brief axis2_any_content_type struct
-    */
+                free)(
+                    struct axis2_any_content_type *any_content_type,
+                    const axis2_env_t *env);
+
+    }
+    axis2_any_content_type_ops_t;
+
+    /**
+     * axis2 any content type struct
+     */
     typedef struct axis2_any_content_type
     {
-        /** axis2_any_content_type related ops */
+        /** operatoins of axis2 any content type struct */
         axis2_any_content_type_ops_t *ops;
-    } axis2_any_content_type_t;
+    }
+    axis2_any_content_type_t;
 
 
-/**
- * creates an instance of any_content_type struct
- * @param env Environment. MUST NOT be NULL.
- * @return Pointer to the newly created any_content_type instance. Returns NULL on error.
- */
-AXIS2_EXTERN axis2_any_content_type_t* AXIS2_CALL 
-axis2_any_content_type_create(const axis2_env_t *env);
-    
-    
-/**************************** Start of function macros ************************/    
+    /**
+     * creates an instance of any_content_type struct
+     * @param env pointer to environment struct. MUST NOT be NULL.
+     * @return Pointer to the newly created any_content_type instance. Returns NULL on error.
+     */
+    AXIS2_EXTERN axis2_any_content_type_t* AXIS2_CALL
+    axis2_any_content_type_create(
+        const axis2_env_t *env);
+
+
+/**************************** Start of function macros ************************/
+
+/** Adds the value.
+    @sa axis2_any_content_type_ops#add_value */
 #define AXIS2_ANY_CONTENT_TYPE_ADD_VALUE(any_content_type, env, qname, value) \
     ((any_content_type)->ops->add_value(any_content_type, env, qname, value))
-    
+
+/** Gets the value.
+    @sa axis2_any_content_type_ops#get_value */
 #define AXIS2_ANY_CONTENT_TYPE_GET_VALUE(any_content_type, env, qname) \
     ((any_content_type)->ops->get_value(any_content_type, env, qname))
-    
+
+/** Gets the value map.
+    @sa axis2_any_content_type_ops#get_value_map */
 #define AXIS2_ANY_CONTENT_TYPE_GET_VALUE_MAP(any_content_type, env) \
     ((any_content_type)->ops->get_value_map(any_content_type, env))
-    
+
+/** Frees the any content type.
+    @sa axis2_any_content_type_ops#free */
 #define AXIS2_ANY_CONTENT_TYPE_FREE(any_content_type, env) \
     ((any_content_type)->ops->free(any_content_type, env))
 
-    
+
 /** @} */
-    
+
 #ifdef __cplusplus
 }
 #endif
