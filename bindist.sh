02f9fccdf120a69dd8963551602924bbad9795d3
Sanjaya Ratnaweera <pini@apache.org>
2006-08-29 11:12:09 +0000

    Added missing files to the tarball created by "make bindist".

--- bindist.sh
+++ bindist.sh
@@ -6,22 +6,21 @@ cd ../axiom
 sh build.sh
 cd ../xml_schema
 sh build.sh
+cd ../woden
+sh build.sh
+
 cd ..
 
-./autogen.sh
+#./autogen.sh
 
 AXIS2C_HOME=`pwd`/deploy
-AXIS2C=`pwd`
-export AXIS2C_HOME AXIS2C
-
-./configure --prefix=${AXIS2C_HOME} --enable-tests=yes --enable-trace=yes  --with-apache2=/usr/include/apache2 --with-apr=/usr/include/apr-0 --enable-diclient=no --enable-static=no --enable-openssl=no --with-axis2_util=${AXIS2C}/util/include --with-axiom=${AXIS2C}/axiom/include
-make
-make install
+export AXIS2C_HOME 
+sh build.sh
+#./configure --prefix=${AXIS2C_HOME} --enable-tests=yes --enable-trace=yes  --with-apache2=/usr/local/apache2/include --with-apr=/usr/include/apr-0 --enable-diclient=no --enable-static=no --enable-openssl=no --with-axis2_util=${AXIS2C}/util/include --with-axiom=${AXIS2C}/axiom/include
 
 cd rampart
 sh build.sh
-cd ../woden
-sh build.sh
+
 cd ..
 rm -rf xdocs/api/html
 maven site
@@ -32,6 +31,8 @@ cp -r xdocs/api/html target/docs/api/
 cp -r target/docs ${AXIS2C_HOME}
 
 cd ${AXIS2C_HOME}
+
+# rm -rf config.sub missing config.guess depcomp ltmain.sh
 for i in `find . -name "*.la"`
 do
    rm $i
