19698ae1de0f42737a439aef33eb2988b246ef1f
Sanjaya Ratnaweera <pini@apache.org>
2006-08-30 06:08:44 +0000

    Added correct samples directory to the binary distribution.

--- bindist.sh
+++ bindist.sh
@@ -11,14 +11,31 @@ sh build.sh
 
 cd ..
 
-#./autogen.sh
-
-AXIS2C_HOME=`pwd`/deploy
-export AXIS2C_HOME 
-sh build.sh
 #./configure --prefix=${AXIS2C_HOME} --enable-tests=yes --enable-trace=yes  --with-apache2=/usr/local/apache2/include --with-apr=/usr/include/apr-0 --enable-diclient=no --enable-static=no --enable-openssl=no --with-axis2_util=${AXIS2C}/util/include --with-axiom=${AXIS2C}/axiom/include
 
-cd rampart
+./autogen.sh
+AXIS2C_HOME=`pwd`/deploy
+AXIS2C=`pwd`
+export AXIS2C_HOME AXIS2C
+
+./configure --prefix=${AXIS2C_HOME} --enable-tests=yes --enable-trace=yes  --enable-diclient=no --enable-static=no --enable-openssl=no --with-axis2_util=${AXIS2C_HOME}/include --with-axiom=${AXIS2C_HOME}/include
+
+make
+make install
+
+cd samples
+
+./autogen.sh
+
+./configure --prefix=${AXIS2C_HOME} --with-axis2_util=${AXIS2C_HOME}/include --with-axiom=${AXIS2C_HOME}/include
+make
+make dist
+tar xf axis2c-sample-src-0.93.tar.gz
+mv axis2c-sample-src-0.93 samples
+rm -rf ${AXIS2C_HOME}/samples
+mv samples ${AXIS2C_HOME}
+
+cd ../rampart
 sh build.sh
 
 cd ..
