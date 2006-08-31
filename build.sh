1649071adda3c74617c777f053669f1c3ee82969
Don Samisa Abeysinghe <samisa@apache.org>
2006-08-31 14:11:53 +0000

    Fixed problems in docs, added news to index and added test to build to fix error

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ AXIS2C_HOME=`pwd`/deploy
 AXIS2C=`pwd`
 export AXIS2C_HOME AXIS2C
 
-./configure --prefix=${AXIS2C_HOME} --enable-tests=no --enable-trace=no  --enable-diclient=no --enable-static=no --enable-openssl=no --with-axis2_util=${AXIS2C}/util/include --with-axiom=${AXIS2C}/axiom/include --with-apache2=/usr/local/apache2/include --with-apr=/usr/local/apache2/include
+./configure --prefix=${AXIS2C_HOME} --enable-tests=yes --enable-trace=no  --enable-diclient=no --enable-static=no --enable-openssl=no --with-axis2_util=${AXIS2C}/util/include --with-axiom=${AXIS2C}/axiom/include --with-apache2=/usr/local/apache2/include --with-apr=/usr/local/apache2/include
 
 make 
 make install
