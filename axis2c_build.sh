64e99eb790ab30cf35b62c502dd4d4146dbeab69
Sanjaya Ratnaweera <pini@apache.org>
2006-08-31 05:52:48 +0000

    Added the script with excutable rights

---
+++ axis2c_build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+if [ -z "$1" ] ; then
+    AXIS2C_DEPLOY="/usr/local/axis2c"
+else
+    AXIS2C_DEPLOY=$1
+fi
+
+export LD_LIBRARY_PATH=${AXIS2C_DEPLOY}/lib
+cd util
+
+./configure --prefix=${AXIS2C_DEPLOY}
+make 
+make install
+
+cd ../axiom
+./configure --prefix=${AXIS2C_DEPLOY} --with-axis2_util=${AXIS2C_DEPLOY}/include
+make 
+make install
+
+cd ../xml_schema
+./configure --prefix=${AXIS2C_DEPLOY} --with-axis2_util=${AXIS2C_DEPLOY}/include --with-axiom=${AXIS2C_DEPLOY}/include
+make 
+make install
+
+cd ../woden
+./configure --prefix=${AXIS2C_DEPLOY} --with-axis2_util=${AXIS2C_DEPLOY}/include --with-axiom=${AXIS2C_DEPLOY}/include
+make 
+make install
+
+cd ..
+./configure --prefix=${AXIS2C_DEPLOY} --with-axis2_util=${AXIS2C_DEPLOY}/include --with-axiom=${AXIS2C_DEPLOY}/include
+make 
+make install
+
+cd rampart
+./configure --prefix=${AXIS2C_DEPLOY} --with-axis2_util=${AXIS2C_DEPLOY}/include --with-axiom=${AXIS2C_DEPLOY}/include
+make 
+make install
+
+cd ../samples
+./configure --prefix=${AXIS2C_DEPLOY} --with-axis2_util=${AXIS2C_DEPLOY}/include --with-axiom=${AXIS2C_DEPLOY}/include
+make 
+make install
+
+
