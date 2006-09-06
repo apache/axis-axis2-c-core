a51afb41ccb6b43731fc7bf12b63515c4b29b6bd
Don Samisa Abeysinghe <samisa@apache.org>
2006-09-06 05:53:23 +0000

    Fixed the build system so that it would not assume CuTest

--- axiom/build.sh
+++ axiom/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ./autogen.sh
-./configure --prefix=${AXIS2C_HOME} --enable-static=no --with-axis2_util=${AXIS2C_HOME}/include --enable-tests=yes
+./configure --prefix=${AXIS2C_HOME} --enable-static=no --with-axis2_util=${AXIS2C_HOME}/include --enable-tests=no
 make 
 make install
 
--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ AXIS2C_HOME=`pwd`/deploy
 AXIS2C=`pwd`
 export AXIS2C_HOME AXIS2C
 
-./configure --prefix=${AXIS2C_HOME} --enable-tests=yes --enable-trace=yes  --enable-diclient=no --enable-static=no --enable-openssl=no --with-axis2_util=${AXIS2C}/util/include --with-axiom=${AXIS2C}/axiom/include 
+./configure --prefix=${AXIS2C_HOME} --enable-tests=no --enable-trace=yes  --enable-diclient=no --enable-static=no --enable-openssl=no --with-axis2_util=${AXIS2C}/util/include --with-axiom=${AXIS2C}/axiom/include 
 
 make 
 make install
