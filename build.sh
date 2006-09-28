4ac9eb906bb963a7c3a88c0c64578379cf9803a7
Sanjaya Ratnaweera <pini@apache.org>
2006-09-28 05:34:31 +0000

    Fix for the jira 250.

--- build.sh
+++ build.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 ./autogen.sh
 AXIS2C_HOME=${AXIS2C_HOME:=`pwd`/deploy}
-AXIS2C=${AXIS2C:=`pwd`}
 
-export AXIS2C_HOME AXIS2C
+export AXIS2C_HOME
 
 echo "AXIS2C_HOME = ${AXIS2C_HOME}"
-echo "AXIS2       = ${AXIS2C}"
 
-./configure --prefix=${AXIS2C_HOME} --enable-tests=no --enable-trace=yes  --enable-diclient=no --enable-static=no --enable-openssl=no --with-axis2_util=${AXIS2C}/util/include --with-axiom=${AXIS2C}/axiom/include 
-
+./configure --prefix=${AXIS2C_HOME} 
 make 
 make install
+
 cd samples
 ./autogen.sh
 ./configure --prefix=${AXIS2C_HOME} --with-axis2_util=${AXIS2C_HOME}/include --with-axiom=${AXIS2C_HOME}/include
