3cd75cec0918cdcef30d7f71de7f40e98fd85e26
Weerapurage Dinesh Premalal <dinesh@apache.org>
2006-10-19 12:56:07 +0000

    patch submitted by Manjula

--- modules/mod_log/log_in_handler.c
+++ modules/mod_log/log_in_handler.c
@@ -77,6 +77,7 @@ axis2_log_in_handler_invoke(struct axis2_handler *handler,
     
     if (soap_envelope)
     {
+        AXIOM_SOAP_ENVELOPE_GET_BODY(soap_envelope, env); /* hack to ensure soab buider state is in sync */
         ret_node = AXIOM_SOAP_ENVELOPE_GET_BASE_NODE(soap_envelope, env);
 
         if(ret_node)
