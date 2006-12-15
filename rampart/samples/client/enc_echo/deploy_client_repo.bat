8a3f2c5dd9ff6cd1019b2ecac95e7cf775abc0d5
Malinda Kaushalye Kapuruge <kaushalye@apache.org>
2006-12-15 12:51:16 +0000

    Committing changes made to win32 build

--- rampart/samples/client/enc_echo/deploy_client_repo.bat
+++ rampart/samples/client/enc_echo/deploy_client_repo.bat
@@ -1,7 +1,7 @@
 echo off
 
 rem if your client repository is different, change the value.
-set CLIENT_REPO=%HOMEDRIVE%%HOMEPATH%\clientrepo
+set CLIENT_REPO=%AXIS2C_HOME%\clientrepo
 
 echo Start creating a client repository at %CLIENT_REPO%
 
