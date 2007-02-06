ba43ac36336bd690cea14343eac4bef7f6668065
Malinda Kaushalye Kapuruge <kaushalye@apache.org>
2007-02-06 11:48:56 +0000

    Adding Signature Parts to OMXMLSecurity

---
+++ rampart/src/omxmlsec/sign_part.c
@@ -0,0 +1,168 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include <axis2_util.h>
+#include <oxs_sign_part.h>
+#include <oxs_error.h>
+
+struct oxs_sign_part_t
+{
+    axis2_char_t *id;
+    axis2_char_t *digest_mtd;
+    axiom_node_t *node ; /*Shallow copies*/
+    axis2_array_list_t *transforms; /*Shallow copies*/
+};
+
+
+/*Public functions*/
+axis2_char_t *AXIS2_CALL
+oxs_sign_part_get_id(
+    const oxs_sign_part_t *sign_part,
+    const axis2_env_t *env)
+{
+    return sign_part->id;
+}
+
+axis2_char_t *AXIS2_CALL
+oxs_sign_part_get_digest_mtd(
+    const oxs_sign_part_t *sign_part,
+    const axis2_env_t *env)
+{
+    return sign_part->digest_mtd;
+}
+
+axiom_node_t *AXIS2_CALL
+oxs_sign_part_get_node(
+    const oxs_sign_part_t *sign_part,
+    const axis2_env_t *env)
+{
+    return sign_part->node;
+}
+
+axis2_array_list_t *AXIS2_CALL
+oxs_sign_part_get_transforms(
+    const oxs_sign_part_t *sign_part,
+    const axis2_env_t *env)
+{
+    return sign_part->transforms;
+}
+
+axis2_status_t AXIS2_CALL
+oxs_sign_part_set_id(
+    oxs_sign_part_t *sign_part,
+    const axis2_env_t *env,
+    axis2_char_t *id)
+{
+
+    if (sign_part->id)
+    {
+        AXIS2_FREE(env->allocator, sign_part->id);
+        sign_part->id = NULL;
+    }
+    sign_part->id = AXIS2_STRDUP(id, env);
+    return AXIS2_SUCCESS;
+}
+
+axis2_status_t AXIS2_CALL
+oxs_sign_part_set_digest_mtd(
+    oxs_sign_part_t *sign_part,
+    const axis2_env_t *env,
+    axis2_char_t *digest_mtd)
+{
+
+    if (sign_part->digest_mtd)
+    {
+        AXIS2_FREE(env->allocator, sign_part->digest_mtd);
+        sign_part->digest_mtd = NULL;
+    }
+    sign_part->digest_mtd = AXIS2_STRDUP(digest_mtd, env);
+    return AXIS2_SUCCESS;
+}
+
+axis2_status_t AXIS2_CALL
+oxs_sign_part_set_node(
+    oxs_sign_part_t *sign_part,
+    const axis2_env_t *env,
+    axiom_node_t *node)
+{
+    sign_part->node = node;
+    return AXIS2_SUCCESS;
+}
+
+axis2_status_t AXIS2_CALL
+oxs_sign_part_set_transforms(
+    oxs_sign_part_t *sign_part,
+    const axis2_env_t *env,
+    axis2_array_list_t *transforms)
+{
+    sign_part->transforms = transforms;
+    return AXIS2_SUCCESS;
+}
+
+AXIS2_EXTERN oxs_sign_part_t *AXIS2_CALL
+oxs_sign_part_create(const axis2_env_t *env)
+{
+    oxs_sign_part_t *sign_part = NULL;
+
+    AXIS2_ENV_CHECK(env, NULL);
+
+    sign_part = AXIS2_MALLOC(env->allocator, sizeof(oxs_sign_part_t));
+    if (!sign_part)
+    {
+        AXIS2_ERROR_SET(env->error, AXIS2_ERROR_NO_MEMORY, AXIS2_FAILURE);
+        return NULL;
+    }
+
+    sign_part->id= NULL;
+    sign_part->digest_mtd = NULL;
+    sign_part->node = NULL;
+    sign_part->transforms = NULL;
+
+    return sign_part;
+}
+
+
+axis2_status_t AXIS2_CALL
+oxs_sign_part_free(oxs_sign_part_t *sign_part,
+        const axis2_env_t *env)
+{
+
+    AXIS2_ENV_CHECK(env, AXIS2_FAILURE);
+
+    if (sign_part->id)
+    {
+        AXIS2_FREE(env->allocator, sign_part->id);
+        sign_part->id = NULL;
+    }
+
+    if (sign_part->digest_mtd)
+    {
+        AXIS2_FREE(env->allocator, sign_part->digest_mtd);
+        sign_part->digest_mtd = NULL;
+    }
+
+    sign_part->node = NULL;
+    sign_part->transforms = NULL;
+
+    AXIS2_FREE(env->allocator,  sign_part);
+    sign_part = NULL;
+
+    return AXIS2_SUCCESS;
+}
+
+
