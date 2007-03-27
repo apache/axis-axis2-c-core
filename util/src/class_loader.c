c4f0b79b8e902d7d68db15ac38f93d9148e95a0f
Don Samisa Abeysinghe <samisa@apache.org>
2007-03-27 01:42:02 +0000

    Removed unwanted if blocks - AXIS2C-170

--- util/src/class_loader.c
+++ util/src/class_loader.c
@@ -52,19 +52,12 @@ AXIS2_EXTERN void * AXIS2_CALL
 axis2_class_loader_create_dll(const axis2_env_t *env,
         axis2_param_t *impl_info_param)
 {
-    void *svc_skeli = NULL; /* axis2_svc_skeleton */
-    void *msg_recv = NULL; /* axis2_msg_recv */
-    void *handler = NULL; /* axis2_handler */
-    void *module = NULL; /* axis2_module */
-    void *transport_recv = NULL; /* axis2_transport_receiver */
-    void *transport_sender = NULL; /* axis2_transport_sender */
     void *obj = NULL;
     CREATE_FUNCT create_funct = NULL;
     DELETE_FUNCT delete_funct = NULL;
     AXIS2_DLHANDLER dl_handler = NULL;
     axis2_dll_desc_t *dll_desc = NULL;
     axis2_status_t status = AXIS2_FAILURE;
-    axis2_dll_type_t dll_type = AXIS2_SVC_DLL;
     axis2_error_codes_t error_code = AXIS2_ERROR_NONE;
 
     dll_desc = axis2_param_get_value(impl_info_param, env);
@@ -127,116 +120,21 @@ axis2_class_loader_create_dll(const axis2_env_t *env,
                 AXIS2_FAILURE);
         return NULL;
     }
-    dll_type =  axis2_dll_desc_get_type(dll_desc, env);
-    /* TODO Following blocks differ only by error message. Try to
-     * reduce them into one block
-     */
     error_code =  axis2_dll_desc_get_error_code(dll_desc, env) ;
-    if (AXIS2_SVC_DLL == dll_type)
+
+    create_funct(&obj, env);
+    if (! obj)
     {
-        create_funct(&svc_skeli, env);
-        if (! svc_skeli)
-        {
-            axis2_class_loader_unload_lib(env, dll_desc);
-            AXIS2_ERROR_SET(env->error,
-                    AXIS2_ERROR_SVC_SKELETON_CREATION_FAILED, AXIS2_FAILURE);
-            return NULL;
-        }
-        return svc_skeli;
-    }
-    if (AXIS2_HANDLER_DLL == dll_type)
-    {
-        create_funct(&handler, env);
-        if (! handler)
-        {
-            axis2_class_loader_unload_lib(env, dll_desc);
-            AXIS2_ERROR_SET(env->error,
-                    AXIS2_ERROR_HANDLER_CREATION_FAILED, AXIS2_FAILURE);
-            return NULL;
-        }
-        return handler;
-    }
-    if (AXIS2_MODULE_DLL == dll_type)
-    {
-        create_funct(&module, env);
-        if (! module)
-        {
-            axis2_class_loader_unload_lib(env, dll_desc);
-            AXIS2_ERROR_SET(env->error,
-                    AXIS2_ERROR_MODULE_CREATION_FAILED, AXIS2_FAILURE);
-            return NULL;
-        }
-        return module;
-    }
-    if (AXIS2_MSG_RECV_DLL == dll_type)
-    {
-        create_funct(&msg_recv, env);
-        if (! msg_recv)
-        {
-            axis2_class_loader_unload_lib(env, dll_desc);
-            AXIS2_LOG_DEBUG(env->log, AXIS2_LOG_SI, "message receiver is NULL");
-            AXIS2_ERROR_SET(env->error,
-                    AXIS2_ERROR_MSG_RECV_CREATION_FAILED, AXIS2_FAILURE);
-            return NULL;
-        }
-        else
-        {
-            AXIS2_LOG_DEBUG(env->log, AXIS2_LOG_SI, "message receiver loaded successfully");
-        }
-        return msg_recv;
-    }
-    if (AXIS2_TRANSPORT_RECV_DLL == dll_type)
-    {
-        create_funct(&transport_recv, env);
-        if (! transport_recv)
-        {
-            axis2_class_loader_unload_lib(env, dll_desc);
-            AXIS2_LOG_DEBUG(env->log, AXIS2_LOG_SI, "transport receiver is NULL");
-            AXIS2_ERROR_SET(env->error,
-                    AXIS2_ERROR_TRANSPORT_RECV_CREATION_FAILED, AXIS2_FAILURE);
-            return NULL;
-        }
-        else
-        {
-            AXIS2_LOG_DEBUG(env->log, AXIS2_LOG_SI, "transport receiver loaded successfully");
-        }
-        return transport_recv;
-    }
-    if (AXIS2_TRANSPORT_SENDER_DLL == dll_type)
-    {
-        create_funct(&transport_sender, env);
-        if (! transport_sender)
-        {
-            axis2_class_loader_unload_lib(env, dll_desc);
-            AXIS2_LOG_DEBUG(env->log, AXIS2_LOG_SI, "transport sender is NULL");
-            AXIS2_ERROR_SET(env->error,
-                    AXIS2_ERROR_TRANSPORT_SENDER_CREATION_FAILED, AXIS2_FAILURE);
-            return NULL;
-        }
-        else
-        {
-            AXIS2_LOG_DEBUG(env->log, AXIS2_LOG_SI, "transport sender loaded successfully");
-        }
-        return transport_sender;
+        axis2_class_loader_unload_lib(env, dll_desc);
+        AXIS2_LOG_DEBUG(env->log, AXIS2_LOG_SI, "Object create function returned NULL");
+        AXIS2_ERROR_SET(env->error, error_code, AXIS2_FAILURE);
+        return NULL;
     }
     else
     {
-        create_funct(&obj, env);
-        if (! obj)
-        {
-            axis2_class_loader_unload_lib(env, dll_desc);
-            AXIS2_LOG_DEBUG(env->log, AXIS2_LOG_SI, "Object create function returned NULL");
-            AXIS2_ERROR_SET(env->error, error_code, AXIS2_FAILURE);
-            return NULL;
-        }
-        else
-        {
-            AXIS2_LOG_DEBUG(env->log, AXIS2_LOG_SI, "Object loaded successfully");
-        }
-        return obj;
+        AXIS2_LOG_DEBUG(env->log, AXIS2_LOG_SI, "Object loaded successfully");
     }
-
-    return NULL;
+    return obj;
 }
 
 axis2_status_t
