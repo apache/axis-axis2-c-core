d58ec1b4c9d8119a7852f5a10dbf165568814994
Don Samisa Abeysinghe <samisa@apache.org>
2007-04-03 06:40:55 +0000

    Moving woden to scratch

--- woden/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-./autogen.sh
-
-./configure --prefix=${AXIS2C_HOME} --enable-static=no --with-axis2_util=${AXIS2C_HOME}/include --with-axis2_axiom=${AXIS2C_HOME}/include --with-axis2_xml_schema=${AXIS2C_HOME}/include
-make
-make install
