2c2deb9eeac9bd81f00591f49a9f6e97f76905ee
Don Samisa Abeysinghe <samisa@apache.org>
2007-04-03 07:04:09 +0000

    Moved codegen tool to scratch

--- tools/codegen/test/unit/codegen/w2c_writer_test.h
+++
@@ -1,9 +0,0 @@
-#ifndef W2C_WRITER_TEST_H
-#define W2C_WRITER_TEST_H
-
-#include <CuTest.h>
-
-CuSuite* w2c_writerGetSuite();
-
-#endif /* W2C_WRITER_TEST_H */
-
--- tools/codegen/test/unit/util/w2c_conf_test.h
+++
@@ -1,9 +0,0 @@
-#ifndef CODEGEN_CONF_TEST_H
-#define CODEGEN_CONF_TEST_H
-
-#include <CuTest.h>
-
-CuSuite* w2c_confGetSuite();
-
-#endif /* OM_CODEGEN_CONF_H */
-
