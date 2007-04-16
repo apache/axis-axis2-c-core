888bcffdb89b0e5349410272e0acb4fca315303a
Pallewela Perera <dumindu@apache.org>
2007-04-16 08:23:14 +0000

    removing a mistakenly added configure option

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ export AXIS2C_HOME
 
 echo "AXIS2C_HOME = ${AXIS2C_HOME}"
 
-sh configure --with-apache2=/home/dummy/tmp/httpd-2.2.3/deploy/include --prefix=${AXIS2C_HOME}
+sh configure --prefix=${AXIS2C_HOME}
 make 
 make install
 
