3b69b4da55183b891ae9c8ab5ec2abb80e03de43
Don Samisa Abeysinghe <samisa@apache.org>
2007-04-26 04:39:09 +0000

    Fixed module.xml not going with packages

--- bindist.sh
+++ bindist.sh
@@ -38,6 +38,7 @@ cd xdocs/api
 doxygen doxygenconf
 cd ../..
 cp -r xdocs/api/html target/docs/api/
+cp xdocs/docs/mod_log/module.xml target/docs/docs/mod_log
 cp -r target/docs ${AXIS2C_HOME}
 
 cd ${AXIS2C_HOME}
--- dist.sh
+++ dist.sh
@@ -6,6 +6,7 @@ cd xdocs/api
 doxygen doxygenconf
 cd ../..
 cp -r xdocs/api/html target/docs/api/
+cp xdocs/docs/mod_log/module.xml target/docs/docs/mod_log
 cp -r target/docs axis2c-src-1.0.0
 cd samples
 make dist
