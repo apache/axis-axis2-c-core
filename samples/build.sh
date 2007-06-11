45aae06c3a32958f217075fcf5faaf13db0f2331
Sanjaya Ratnaweera <pini@apache.org>
2007-06-11 08:39:33 +0000

    Fixed a error in include path

--- samples/build.sh
+++ samples/build.sh
@@ -2,7 +2,7 @@
 
 ./autogen.sh
 
-./configure --prefix=${AXIS2C_HOME} --with-axis2_util=${AXIS2C_HOME}/include/axis2-1.1 --with-axiom=${AXIS2C_HOME}/include/axis2-1.1
+./configure --prefix=${AXIS2C_HOME} --with-axis2=${AXIS2C_HOME}/include/axis2-1.1
 make 
 make install
 
