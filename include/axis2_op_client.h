4dceaf077a1eed97c4c5fe563709167fd9a04aea
Damitha N.M. Kumarage <damitha@apache.org>
2007-07-23 05:52:21 +0000

    Fixed AXIS2C634

--- include/axis2_op_client.h
+++ include/axis2_op_client.h
@@ -370,6 +370,11 @@ extern "C"
     axis2_op_client_get_svc_ctx(const axis2_op_client_t *op_client,
         const axutil_env_t *env);
 
+    AXIS2_EXTERN void AXIS2_CALL
+    axis2_op_client_set_reuse(
+        axis2_op_client_t *op_client,
+        const axutil_env_t *env,
+        axis2_bool_t reuse);
 
     /**
      * Sends a message represented by the given message context and captures
@@ -394,6 +399,7 @@ extern "C"
     AXIS2_EXTERN axis2_msg_ctx_t *AXIS2_CALL
     axis2_op_client_receive(const axutil_env_t *env,
         axis2_msg_ctx_t *msg_ctx);
+
 
 /** @} */
 #ifdef __cplusplus
