5c630783778060c900c8086e5a6b809a778bf425
Weerapurage Dinesh Premalal <dinesh@apache.org>
2007-07-23 06:02:45 +0000

    removed mistakenly pasted value

--- include/axis2_tcp_transport_sender.h
+++ include/axis2_tcp_transport_sender.h
@@ -1,4 +1,4 @@
-math/math_client.cmath/math_client.c/*
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
