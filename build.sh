ad2502193027310c17885363deaad208fcaf07b0
Don Samisa Abeysinghe <samisa@apache.org>
2007-08-31 04:42:08 +0000

    used -j option for make to make it faster

--- build.sh
+++ build.sh
@@ -8,13 +8,13 @@ export AXIS2C_HOME
 echo "AXIS2C_HOME = ${AXIS2C_HOME}"
 
 sh configure --prefix=${AXIS2C_HOME} 
-make 
+make -j 10 
 make install
 
 cd samples
 sh autogen.sh
 sh configure --prefix=${AXIS2C_HOME} --with-axis2=${AXIS2C_HOME}/include/axis2-1.1
-make 
+make -j 10
 make install
 								
 cd ..
