f2b5a7ddae073e19f39c0ba83bce925b2b5854aa
Don Samisa Abeysinghe <samisa@apache.org>
2007-09-24 03:11:34 +0000

    Fixed the issues raised during release vote

--- build.sh
+++ build.sh
@@ -7,14 +7,14 @@ export AXIS2C_HOME
 
 echo "AXIS2C_HOME = ${AXIS2C_HOME}"
 
-sh configure --prefix=${AXIS2C_HOME} 
-make -j 10 
+sh configure --prefix=${AXIS2C_HOME} --enable-tests=yes --with-apache2=/usr/local/apache2/include --with-apr=/usr/local/apache2/include --enable-tcp
+make 
 make install
 
 cd samples
 sh autogen.sh
 sh configure --prefix=${AXIS2C_HOME} --with-axis2=${AXIS2C_HOME}/include/axis2-1.1
-make -j 10
+make 
 make install
 								
 cd ..
