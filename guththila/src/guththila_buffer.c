94c925b80669c46cd47555343dedb737a0502162
Sanjaya Ratnaweera <pini@apache.org>
2007-10-19 09:38:45 +0000

    Removed junk characters

--- guththila/src/guththila_buffer.c
+++ guththila/src/guththila_buffer.c
@@ -19,46 +19,46 @@
 #include <stdlib.h>
 #include <string.h>
 #include <guththila_buffer.h>
-    int GUTHTHILA_CALL
+    int GUTHTHILA_CALL
 guththila_buffer_init(
     guththila_buffer_t * buffer,
     int size,
     const axutil_env_t * env) 
 {
-    buffer->type = GUTHTHILA_MULTIPLE_BUFFER;
-    buffer->data_size =
+    buffer->type = GUTHTHILA_MULTIPLE_BUFFER;
+    buffer->data_size =
         (size_t *) AXIS2_MALLOC(env->allocator,
                                 sizeof(size_t) *
                                 GUTHTHILA_BUFFER_NUMBER_OF_BUFFERS);
-    buffer->buffs_size =
+    buffer->buffs_size =
         (size_t *) AXIS2_MALLOC(env->allocator,
                                 sizeof(size_t) *
                                 GUTHTHILA_BUFFER_NUMBER_OF_BUFFERS);
-    buffer->buff =
+    buffer->buff =
         (char **) AXIS2_MALLOC(env->allocator,
                                sizeof(char *) *
                                GUTHTHILA_BUFFER_NUMBER_OF_BUFFERS);
-    buffer->cur_buff = -1;
-    buffer->pre_tot_data = 0;
-    buffer->no_buffers = GUTHTHILA_BUFFER_NUMBER_OF_BUFFERS;
-    if (size > 0)
+    buffer->cur_buff = -1;
+    buffer->pre_tot_data = 0;
+    buffer->no_buffers = GUTHTHILA_BUFFER_NUMBER_OF_BUFFERS;
+    if (size > 0)
     {
-        buffer->buff[0] =
+        buffer->buff[0] =
             (guththila_char *) AXIS2_MALLOC(env->allocator,
                                             sizeof(guththila_char) * size);
-        buffer->data_size[0] = 0;
-        buffer->buffs_size[0] = size;
-        buffer->cur_buff = 0;
-    }
-    return GUTHTHILA_SUCCESS;
-}
-int GUTHTHILA_CALL
+        buffer->data_size[0] = 0;
+        buffer->buffs_size[0] = size;
+        buffer->cur_buff = 0;
+    }
+    return GUTHTHILA_SUCCESS;
+}
+int GUTHTHILA_CALL
 guththila_buffer_un_init(
     guththila_buffer_t * buffer,
     const axutil_env_t * env) 
 {
-    int i = 0;
-    if (buffer->type == GUTHTHILA_SINGLE_BUFFER && buffer->buff &&
+    int i = 0;
+    if (buffer->type == GUTHTHILA_SINGLE_BUFFER && buffer->buff &&
          buffer->cur_buff == 0)
     {
         
@@ -67,69 +67,69 @@ guththila_buffer_un_init(
             /*if (buffer->buff[0]) AXIS2_FREE(env->allocator, buffer->buff[0]); */ 
             if (buffer->buffs_size)
             AXIS2_FREE(env->allocator, buffer->buffs_size);
-        if (buffer->data_size)
+        if (buffer->data_size)
             AXIS2_FREE(env->allocator, buffer->data_size);
-        AXIS2_FREE(env->allocator, buffer->buff);
-    }
+        AXIS2_FREE(env->allocator, buffer->buff);
+    }
     else if (buffer->type == GUTHTHILA_MULTIPLE_BUFFER && buffer->buff)
     {
-        for (i = 0; i <= buffer->cur_buff; i++)
+        for (i = 0; i <= buffer->cur_buff; i++)
         {
-            AXIS2_FREE(env->allocator, buffer->buff[i]);
-        }
-        AXIS2_FREE(env->allocator, buffer->buff);
-        if (buffer->data_size)
+            AXIS2_FREE(env->allocator, buffer->buff[i]);
+        }
+        AXIS2_FREE(env->allocator, buffer->buff);
+        if (buffer->data_size)
             AXIS2_FREE(env->allocator, buffer->data_size);
-        if (buffer->buffs_size)
+        if (buffer->buffs_size)
             AXIS2_FREE(env->allocator, buffer->buffs_size);
-    }
-    return GUTHTHILA_SUCCESS;
-}
-int GUTHTHILA_CALL
+    }
+    return GUTHTHILA_SUCCESS;
+}
+int GUTHTHILA_CALL
 guththila_buffer_init_for_buffer(
     guththila_buffer_t * buffer,
     char *buff,
     int size,
     const axutil_env_t * env) 
 {
-    buffer->type = GUTHTHILA_SINGLE_BUFFER;
-    buffer->buff =
+    buffer->type = GUTHTHILA_SINGLE_BUFFER;
+    buffer->buff =
         (char **) AXIS2_MALLOC(env->allocator,
                                sizeof(char *) * GUTHTHILA_BUFFER_DEF_SIZE);
-    buffer->buff[0] = buff;
-    buffer->cur_buff = 0;
-    buffer->buffs_size =
+    buffer->buff[0] = buff;
+    buffer->cur_buff = 0;
+    buffer->buffs_size =
         (size_t *) AXIS2_MALLOC(env->allocator,
                                 sizeof(size_t) * GUTHTHILA_BUFFER_DEF_SIZE);
-    buffer->buffs_size[0] = size;
-    buffer->pre_tot_data = 0;
-    buffer->data_size =
+    buffer->buffs_size[0] = size;
+    buffer->pre_tot_data = 0;
+    buffer->data_size =
         (size_t *) AXIS2_MALLOC(env->allocator,
                                 sizeof(size_t) * GUTHTHILA_BUFFER_DEF_SIZE);
-    buffer->data_size[0] = size;
-    buffer->no_buffers = 1;
-    return GUTHTHILA_SUCCESS;
-}
-void *GUTHTHILA_CALL
+    buffer->data_size[0] = size;
+    buffer->no_buffers = 1;
+    return GUTHTHILA_SUCCESS;
+}
+void *GUTHTHILA_CALL
 guththila_buffer_get(
     guththila_buffer_t * buffer,
     const axutil_env_t * env) 
 {
-    size_t size = 0, current_size = 0;
-    int i = 0;
-    char *buff = NULL;
-    for (i = 0; i <= buffer->cur_buff; i++)
+    size_t size = 0, current_size = 0;
+    int i = 0;
+    char *buff = NULL;
+    for (i = 0; i <= buffer->cur_buff; i++)
     {
-        size += buffer->data_size[i];
-    }
-    buff = (char *) AXIS2_MALLOC(env->allocator, sizeof(char) * (size + 1));
-    for (i = 0; i <= buffer->cur_buff; i++)
+        size += buffer->data_size[i];
+    }
+    buff = (char *) AXIS2_MALLOC(env->allocator, sizeof(char) * (size + 1));
+    for (i = 0; i <= buffer->cur_buff; i++)
     {
-        memcpy(buff + current_size, buffer->buff[i], buffer->data_size[i]);
-        current_size += buffer->data_size[i];
-    }
-    buff[current_size] = '\0';
-    return buff;
-}
+        memcpy(buff + current_size, buffer->buff[i], buffer->data_size[i]);
+        current_size += buffer->data_size[i];
+    }
+    buff[current_size] = '\0';
+    return buff;
+}
 
-
+
--- guththila/src/guththila_namespace.c
+++ guththila/src/guththila_namespace.c
@@ -20,128 +20,128 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-    int GUTHTHILA_CALL
+    int GUTHTHILA_CALL
 guththila_namespace_list_grow(
     guththila_namespace_list_t * namesp_list,
     int addition,
     const axutil_env_t * env) 
 {
-    int i = 0;
-    if (addition > 0 ||
+    int i = 0;
+    if (addition > 0 ||
          (addition < 0 && namesp_list->capacity + addition > 0 &&
           namesp_list->capacity + addition >= namesp_list->size))
     {
-        namesp_list->list =
+        namesp_list->list =
             (guththila_namespace_t *) realloc(namesp_list->list,
                                               sizeof(guththila_namespace_t) *
                                               (namesp_list->capacity +
                                                addition));
-        if (namesp_list->list)
+        if (namesp_list->list)
         {
-            for (i = namesp_list->capacity;
+            for (i = namesp_list->capacity;
                   i < namesp_list->capacity + addition; i++)
             {
-                guththila_stack_push(&namesp_list->fr_stack,
+                guththila_stack_push(&namesp_list->fr_stack,
                                       namesp_list->list + i, env);
-            }
-            namesp_list->capacity += addition;
-        }
+            }
+            namesp_list->capacity += addition;
+        }
         else
         {
-            return GUTHTHILA_FAILURE;
-        }
-    }
-    return 0;
-}
+            return GUTHTHILA_FAILURE;
+        }
+    }
+    return 0;
+}
 
-guththila_namespace_list_t *
+guththila_namespace_list_t *
     GUTHTHILA_CALL guththila_namespace_list_create(const axutil_env_t * env) 
 {
-    int i = 0;
-    guththila_namespace_list_t * namesp_list =
+    int i = 0;
+    guththila_namespace_list_t * namesp_list =
         (guththila_namespace_list_t *) AXIS2_MALLOC(env->allocator,
                                                     sizeof
                                                     (guththila_namespace_list_t));
-    if (!namesp_list)
+    if (!namesp_list)
         return NULL;
-    namesp_list->list =
+    namesp_list->list =
         (guththila_namespace_t *) AXIS2_MALLOC(env->allocator,
                                                sizeof(guththila_namespace_t) *
                                                GUTHTHILA_NAMESPACE_DEF_SIZE);
-    if (namesp_list->list &&
+    if (namesp_list->list &&
           guththila_stack_init(&namesp_list->fr_stack, env))
     {
-        namesp_list->capacity = GUTHTHILA_NAMESPACE_DEF_SIZE;
-        namesp_list->size = 0;
-        for (i = 0; i < GUTHTHILA_NAMESPACE_DEF_SIZE; i++)
+        namesp_list->capacity = GUTHTHILA_NAMESPACE_DEF_SIZE;
+        namesp_list->size = 0;
+        for (i = 0; i < GUTHTHILA_NAMESPACE_DEF_SIZE; i++)
         {
-            guththila_stack_push(&namesp_list->fr_stack, namesp_list->list + i,
+            guththila_stack_push(&namesp_list->fr_stack, namesp_list->list + i,
                                   env);
-        }
-        return namesp_list;
-    }
-    return NULL;
-}
-int GUTHTHILA_CALL
+        }
+        return namesp_list;
+    }
+    return NULL;
+}
+int GUTHTHILA_CALL
 guththila_namespace_list_init(
     guththila_namespace_list_t * namesp_list,
     const axutil_env_t * env) 
 {
-    int i = 0;
-    namesp_list->list =
+    int i = 0;
+    namesp_list->list =
         (guththila_namespace_t *) AXIS2_MALLOC(env->allocator,
                                                sizeof(guththila_namespace_t) *
                                                GUTHTHILA_NAMESPACE_DEF_SIZE);
-    if (namesp_list->list &&
+    if (namesp_list->list &&
           guththila_stack_init(&namesp_list->fr_stack, env))
     {
-        namesp_list->capacity = GUTHTHILA_NAMESPACE_DEF_SIZE;
-        namesp_list->size = 0;
-        for (i = 0; i < GUTHTHILA_NAMESPACE_DEF_SIZE; i++)
+        namesp_list->capacity = GUTHTHILA_NAMESPACE_DEF_SIZE;
+        namesp_list->size = 0;
+        for (i = 0; i < GUTHTHILA_NAMESPACE_DEF_SIZE; i++)
         {
-            guththila_stack_push(&namesp_list->fr_stack, namesp_list->list + i,
+            guththila_stack_push(&namesp_list->fr_stack, namesp_list->list + i,
                                   env);
-        }
-        return GUTHTHILA_SUCCESS;
-    }
-    return GUTHTHILA_FAILURE;
-}
+        }
+        return GUTHTHILA_SUCCESS;
+    }
+    return GUTHTHILA_FAILURE;
+}
 
-guththila_namespace_t *
+guththila_namespace_t *
     GUTHTHILA_CALL guththila_namespace_list_get(guththila_namespace_list_t *
                                                 namesp_list,
                                                 const axutil_env_t * env) 
 {
-    if (namesp_list->fr_stack.top > 0 ||
+    if (namesp_list->fr_stack.top > 0 ||
          guththila_namespace_list_grow(namesp_list,
                                        GUTHTHILA_NAMESPACE_DEF_SIZE, env))
     {
-        return guththila_stack_pop(&namesp_list->fr_stack, env);
-    }
-    return NULL;
-}
-int GUTHTHILA_CALL
+        return guththila_stack_pop(&namesp_list->fr_stack, env);
+    }
+    return NULL;
+}
+int GUTHTHILA_CALL
 guththila_namespace_list_release(
     guththila_namespace_list_t * namesp_list,
     guththila_namespace_t * namespace,
     const axutil_env_t * env) 
 {
-    return guththila_stack_push(&namesp_list->fr_stack, namespace, env);
-}
-void GUTHTHILA_CALL
+    return guththila_stack_push(&namesp_list->fr_stack, namespace, env);
+}
+void GUTHTHILA_CALL
 msuila_namespace_list_free_data(
     guththila_namespace_list_t * namesp_list,
     const axutil_env_t * env) 
 {
-    AXIS2_FREE(env->allocator, namesp_list->list);
-    guththila_stack_un_init(&namesp_list->fr_stack, env);
-} void GUTHTHILA_CALL
+    AXIS2_FREE(env->allocator, namesp_list->list);
+    guththila_stack_un_init(&namesp_list->fr_stack, env);
+} void GUTHTHILA_CALL
 
 guththila_namespace_list_free(
     guththila_namespace_list_t * namesp_list,
     const axutil_env_t * env) 
 {
-    AXIS2_FREE(env->allocator, namesp_list->list);
-    guththila_stack_un_init(&namesp_list->fr_stack, env);
-    AXIS2_FREE(env->allocator, namesp_list);
-} 
+    AXIS2_FREE(env->allocator, namesp_list->list);
+    guththila_stack_un_init(&namesp_list->fr_stack, env);
+    AXIS2_FREE(env->allocator, namesp_list);
+} 
