28a9ecdaa9c0b8194bc6024b95fe9fdd3f0ff281
Don Samisa Abeysinghe <samisa@apache.org>
2007-11-12 06:10:35 +0000

    Fixed warning

--- samples/client/mtom/mtom_client.c
+++ samples/client/mtom/mtom_client.c
@@ -229,7 +229,7 @@ process_response_node(
         axiom_text_t *axiom_text = (axiom_text_t*)axiom_node_get_data_element(res_om_node, env);
         data_handler = axiom_text_get_data_handler(axiom_text, env);
 
-        axiom_data_handler_set_file_name(data_handler, env, to_save_name);
+        axiom_data_handler_set_file_name(data_handler, env, (axis2_char_t *)to_save_name);
         axiom_data_handler_write_to(data_handler, env);
     }else if(axiom_node_get_node_type(res_om_node, env) == AXIOM_ELEMENT){
         res_om_ele = axiom_node_get_data_element(res_om_node, env);
