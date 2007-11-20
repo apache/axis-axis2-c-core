c9d5114e1f80ff4448a61fc49104c24f62334c08
Sanjaya Ratnaweera <pini@apache.org>
2007-11-20 05:10:01 +0000

    Changed the version number

--- dist.sh
+++ dist.sh
@@ -7,14 +7,14 @@ doxygen doxygenconf
 cd ../..
 cp -r xdocs/api/html target/docs/api/
 cp xdocs/docs/mod_log/module.xml target/docs/docs/mod_log
-cp -r target/docs axis2c-src-1.1.0
+cp -r target/docs axis2c-src-1.2.0
 
 cd samples   
 make dist   
-tar -xf axis2c-src-1.1.0.tar.gz     
-mv  axis2c-src-1.1.0 ../axis2c-src-1.1.0    
-cd ../axis2c-src-1.1.0      
-mv axis2c-src-1.1.0 samples
+tar -xf axis2c-src-1.2.0.tar.gz     
+mv  axis2c-src-1.2.0 ../axis2c-src-1.2.0    
+cd ../axis2c-src-1.2.0      
+mv axis2c-src-1.2.0 samples
 
 for i in `find . -name "*.la"`
 do
