ee34e1fa36d446d2908f2d234c09be723a85a1ad
Damitha N.M. Kumarage <damitha@apache.org>
2007-11-29 05:20:44 +0000

    updating to axis2c-1.2

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ make install
 
 cd samples
 sh autogen.sh
-sh configure --prefix=${AXIS2C_HOME} --with-axis2=${AXIS2C_HOME}/include/axis2-1.1
+sh configure --prefix=${AXIS2C_HOME} --with-axis2=${AXIS2C_HOME}/include/axis2-1.2
 make -j 10
 make install
 								
