ec05434c009bef4c41d6e3c4d4441351127cdba9
Weerapurage Dinesh Premalal <dinesh@apache.org>
2008-02-01 11:18:18 +0000

    fixed build scripts

--- samples/build.sh
+++ samples/build.sh
@@ -2,7 +2,7 @@
 
 ./autogen.sh
 
-./configure --prefix=${AXIS2C_HOME} --with-axis2=${AXIS2C_HOME}/include/axis2-1.2.1
+./configure --prefix=${AXIS2C_HOME} --with-axis2=${AXIS2C_HOME}/include/axis2-1.3.0
 make 
 make install
 
