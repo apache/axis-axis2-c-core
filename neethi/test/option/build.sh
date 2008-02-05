f02d3534454debac0de3b301870b2c12335ea796
Senaka Fernando <senaka@apache.org>
2008-02-05 14:09:01 +0000

    Removing test for neethi option, as it is not a part of the Axis2/C project

--- neethi/test/option/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gcc options_test.c ../../../util/test/util/create_env.c -g -I$AXIS2C_HOME/include/axis2-1.2 -L$AXIS2C_HOME/lib -lneethi_util -laxutil -laxis2_axiom -laxis2_parser -o options_test 
