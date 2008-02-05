d2bd1d2e7c7b4dbc58feeeb68a28e777746ad7ba
Senaka Fernando <senaka@apache.org>
2008-02-05 13:59:16 +0000

    Adding Milinda's fix to date_time test

--- util/test/date_time/build.sh
+++ util/test/date_time/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc date_time_test.c ../util/create_env.c -g -I$AXIS2C_HOME/include/axis2-1.2 -L$AXIS2C_HOME/lib -laxutil -laxis2_axiom -laxis2_parser -o date_time_test 
+gcc date_time_test.c ../util/create_env.c -g -I$AXIS2C_HOME/include/axis2-1.3.0 -L$AXIS2C_HOME/lib -laxutil -laxis2_axiom -laxis2_parser -o date_time_test  -ldl -Wl,--rpath -Wl,$AXIS2C_HOME/lib 
