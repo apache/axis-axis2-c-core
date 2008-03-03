a12536b14fbadfb0bc45ce818f6b3160c8ce4dbc
Senaka Fernando <senaka@apache.org>
2008-03-03 22:09:47 +0000

    Adding one more fix to suit error cases

--- util/src/uri.c
+++ util/src/uri.c
@@ -390,6 +390,8 @@ axutil_uri_parse_string(
     /* scheme must be non-empty and followed by :// */
     if (s == uri_str || s[0] != ':' || s[1] != '/' || s[2] != '/')
     {
+        s = uri_str; /* restart from beginning as the loop must have ended in
+                      * in a wrong place. */
         uri->scheme = axutil_strdup(env, "http"); /* if no scheme use HTTP */
         goto deal_with_authority;    /* backwards predicted taken! */
     }
