c4068b31bf596310ae404fc8d5561a70d1a5640f
Senaka Fernando <senaka@apache.org>
2008-03-15 10:51:48 +0000

    Fixing JIRA Issue AXIS2C-1054

--- include/axis2_http_transport_utils.h
+++ include/axis2_http_transport_utils.h
@@ -151,6 +151,11 @@ extern "C"
         axis2_conf_ctx_t * conf_ctx);
 
     AXIS2_EXTERN axis2_char_t *AXIS2_CALL
+    axis2_http_transport_utils_get_method_not_allowed(
+        const axutil_env_t * env,
+        axis2_conf_ctx_t * conf_ctx);
+
+    AXIS2_EXTERN axis2_char_t *AXIS2_CALL
 
     axis2_http_transport_utils_get_services_html(
         const axutil_env_t * env,
