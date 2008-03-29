dd814731ad99990bc6e50db73706ce2f9e10d2be
Malinda Kaushalye Kapuruge <kaushalye@apache.org>
2008-03-29 18:23:14 +0000

    hackathon : SOAP fault reason header

--- axiom/include/axiom_soap_fault_reason.h
+++ axiom/include/axiom_soap_fault_reason.h
@@ -43,11 +43,13 @@ extern "C"
      */
 
     /**
-      * creates a soap struct 
+      * creates a SOAP fault reason struct 
       * @param env Environment. MUST NOT be NULL
+      * @param fault the SOAP fault
+      *
+      * @return the created SOAP fault reason struct
       */
     AXIS2_EXTERN axiom_soap_fault_reason_t *AXIS2_CALL
-
     axiom_soap_fault_reason_create_with_parent(
         const axutil_env_t * env,
         axiom_soap_fault_t * fault);
@@ -56,43 +58,74 @@ extern "C"
       * Free an axiom_soap_fault_reason
       * @param  fault_reason pointer to soap_fault_reason struct
       * @param  env Environment. MUST NOT be NULL
-      * @return satus of the op. AXIS2_SUCCESS on success 
-      *         else AXIS2_FAILURE
+      *
+      * @return VOID 
       */
-
     AXIS2_EXTERN void AXIS2_CALL
     axiom_soap_fault_reason_free(
         axiom_soap_fault_reason_t * fault_reason,
         const axutil_env_t * env);
 
+    /**
+      * Get the SOAP fault text by comparing the given string
+      * @param  fault_reason pointer to soap_fault_reason struct
+      * @param  env Environment. MUST NOT be NULL
+      * @param lang string to be compares
+      *
+      * @return the SOAP fault text of the SOAP fault string 
+      */
     AXIS2_EXTERN struct axiom_soap_fault_text *AXIS2_CALL
+    axiom_soap_fault_reason_get_soap_fault_text(
+        axiom_soap_fault_reason_t * fault_reason,
+        const axutil_env_t * env,
+        axis2_char_t * lang);
 
-                axiom_soap_fault_reason_get_soap_fault_text(
-                    axiom_soap_fault_reason_t * fault_reason,
-                    const axutil_env_t * env,
-                    axis2_char_t * lang);
-
+    /**
+      * Returns all the  SOAP fault reason strings as an array list
+      * @param  fault_reason pointer to soap_fault_reason struct
+      * @param  env Environment. MUST NOT be NULL
+      *
+      * @return  all the  SOAP fault reason strings as an array list
+      */
     AXIS2_EXTERN axutil_array_list_t *AXIS2_CALL
-
     axiom_soap_fault_reason_get_all_soap_fault_texts(
         axiom_soap_fault_reason_t * fault_reason,
         const axutil_env_t * env);
 
+    /**
+      * Retuens the first SOAP fault reason string
+      * @param  fault_reason pointer to soap_fault_reason struct
+      * @param  env Environment. MUST NOT be NULL
+      *
+      * @return The first SOAP fault reason string
+      */
     AXIS2_EXTERN struct axiom_soap_fault_text *AXIS2_CALL
+        axiom_soap_fault_reason_get_first_soap_fault_text(
+        axiom_soap_fault_reason_t * fault_reason,
+        const axutil_env_t * env);
 
-                axiom_soap_fault_reason_get_first_soap_fault_text(
-                    axiom_soap_fault_reason_t * fault_reason,
-                    const axutil_env_t * env);
-
+    /**
+      * Add a string as a SOAP fault reason
+      * @param  fault_reason pointer to soap_fault_reason struct
+      * @param  env Environment. MUST NOT be NULL
+      * @param fault_text The text to be added as the SOAP fault reason
+      *
+      * @return satus of the op. AXIS2_SUCCESS on success 
+      *         else AXIS2_FAILURE
+      */
     AXIS2_EXTERN axis2_status_t AXIS2_CALL
-
     axiom_soap_fault_reason_add_soap_fault_text(
         axiom_soap_fault_reason_t * fault_reason,
         const axutil_env_t * env,
         struct axiom_soap_fault_text *fault_text);
 
+    /**
+      * Get the  base node of the SOAP fault reason
+      * @param  fault_reason pointer to soap_fault_reason struct
+      * @param  env Environment. MUST NOT be NULL
+      * @return the base node of the SOAP fault reason 
+      */
     AXIS2_EXTERN axiom_node_t *AXIS2_CALL
-
     axiom_soap_fault_reason_get_base_node(
         axiom_soap_fault_reason_t * fault_reason,
         const axutil_env_t * env);
