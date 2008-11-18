3d8f0fd19eca2ee0e4d738b525668d84d169c7ac
Thelge Peiris <manjula@apache.org>
2008-11-18 06:28:21 +0000

    Applying the patch for AXIS2C-1291

--- samples/server/echo/echo_skeleton.c
+++ samples/server/echo/echo_skeleton.c
@@ -16,6 +16,7 @@
  * limitations under the License.
  */
 #include <axis2_svc_skeleton.h>
+#include <axis2_svc_client.h>
 #include "echo.h"
 #include <axutil_array_list.h>
 #include <axis2_msg_ctx.h>
@@ -92,6 +93,19 @@ echo_invoke(
      * To see how to deal with multiple impl methods, have a look at the
      * math sample.
      */
+	axis2_endpoint_ref_t* to_epr = NULL;
+	to_epr = axis2_msg_ctx_get_to(msg_ctx, env);
+	
+	if (to_epr)
+	{
+		axis2_char_t* to_address = NULL;
+		to_address = axis2_endpoint_ref_get_address(to_epr, env);
+	
+		if (to_address && strstr(to_address, AXIS2_ANON_SERVICE))
+		{
+			axis2_msg_ctx_set_wsa_action(msg_ctx, env, AXIS2_ANON_OUT_IN_OP);
+		}
+	}
 
     return axis2_echo_echo(env, node);
 }
