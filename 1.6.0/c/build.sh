6b195f17b13b260e4818f0748571f3644a79272a
Thelge Peiris <manjula@apache.org>
2009-04-16 09:57:43 +0000

    Copying the trunk to the branch.

---
+++ 1.6.0/c/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+sh autogen.sh
+AXIS2C_HOME=${AXIS2C_HOME:=`pwd`/deploy}
+
+export AXIS2C_HOME
+
+echo "AXIS2C_HOME = ${AXIS2C_HOME}"
+
+sh configure --prefix=${AXIS2C_HOME} --enable-tests=yes
+make -j 10 
+make install
+
+cd samples
+sh autogen.sh
+sh configure --prefix=${AXIS2C_HOME} --with-axis2=${AXIS2C_HOME}/include/axis2-1.6.0
+make -j 10
+make install
+								
+cd ..
